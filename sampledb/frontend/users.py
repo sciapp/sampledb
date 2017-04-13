edabce7daca9ba3770005c4f52fed01878358f65
Florian Rhiem <f.rhiem@fz-juelich.de>
2017-04-13 11:59:43 +0200

    Added user profile placeholder

--- sampledb/frontend/users.py
+++ sampledb/frontend/users.py
@@ -130,6 +130,19 @@ def registration():
     return flask.render_template('index.html')
 
 
+@frontend.route('/users/me')
+@flask_login.login_required
+def current_user_profile():
+    return flask.redirect(flask.url_for('.user_profile', user_id=flask_login.current_user.id))
+
+
+@frontend.route('/users/<int:user_id>')
+@flask_login.login_required
+def user_profile(user_id):
+    # TODO: this is a placeholder for now. user profiles will be implemented in the future.
+    return flask.redirect(flask.url_for('.index'))
+
+
 @frontend.route('/users/me/preferences', methods=['GET', 'POST'])
 @flask_login.login_required
 def user_me_preferences():
