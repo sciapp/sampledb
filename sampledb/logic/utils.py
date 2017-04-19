4723ba18cf5e2e664972848924dad36f870c68ab
Dorothea Henkel <d.henkel@fz-juelich.de>
2017-04-19 14:27:30 +0200

    delete unused files and optimize send_confirm_email  Please enter the commit message for your changes. Lines starting

--- sampledb/logic/utils.py
+++ sampledb/logic/utils.py
@@ -12,13 +12,14 @@ from .security_tokens import generate_token
 
 def send_confirm_email(email, id, salt):
     if id == None:
+        route = "frontend.%s_route" % salt
         token = generate_token(email, salt=salt,
                                secret_key=flask.current_app.config['SECRET_KEY'])
-        confirm_url = flask.url_for("frontend.invitation_route", token=token, salt=salt, _external=True)
+        confirm_url = flask.url_for(route, token=token, salt=salt, _external=True)
     else:
         token = generate_token([email, id], salt=salt,
                                secret_key=flask.current_app.config['SECRET_KEY'])
-        confirm_url = flask.url_for("frontend.confirm_email", token=token, salt=salt, _external=True)
+        confirm_url = flask.url_for("frontend.user_preferences", user_id=id, token=token, salt=salt, _external=True)
 
     subject = "Please confirm your email"
     html = flask.render_template('activate.html', confirm_url=confirm_url)
