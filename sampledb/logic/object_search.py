9bb943459bfd833948324887e3522c6cd49b9213
Florian Rhiem <f.rhiem@fz-juelich.de>
2017-05-17 14:04:53 +0200

    Bugfix: used redefined errors variable instead of module

--- sampledb/logic/object_search.py
+++ sampledb/logic/object_search.py
@@ -4,11 +4,21 @@
 """
 
 import json
+import typing
 from sqlalchemy import String
 from . import where_filters
 
 
-def generate_filter_func(query_string):
+def generate_filter_func(query_string: str) -> typing.Callable:
+    """
+    Generates a filter function for use with SQLAlchemy and the JSONB data
+    attribute in the object tables.
+
+    The generated filter functions can be used for objects.get_objects()
+
+    :param query_string: the query string
+    :return: filter func
+    """
     if query_string:
         def filter_func(data, query_string=query_string):
             """ Filter objects based on search query string """
