7c975b5973aa4dcc74fe1aac1f49432ed3e87142
Florian Rhiem <f.rhiem@fz-juelich.de>
2018-02-01 15:09:39 +0100

    Added support for instrument-independent actions to create_action script

--- sampledb/scripts/create_action.py
+++ sampledb/scripts/create_action.py
@@ -19,11 +19,14 @@ def main(arguments):
         print(__doc__)
         exit(1)
     instrument_id, action_type, name, description, schema_file_name = arguments
-    try:
-        instrument_id = int(instrument_id)
-    except ValueError:
-        print("Error: instrument_id must be an integer", file=sys.stderr)
-        exit(1)
+    if instrument_id == 'None':
+        instrument_id = None
+    else:
+        try:
+            instrument_id = int(instrument_id)
+        except ValueError:
+            print("Error: instrument_id must be an integer or 'None'", file=sys.stderr)
+            exit(1)
     if action_type == 'sample':
         action_type = ActionType.SAMPLE_CREATION
     elif action_type == 'measurement':
@@ -35,11 +38,12 @@ def main(arguments):
         exit(1)
     app = create_app()
     with app.app_context():
-        try:
-            instrument = get_instrument(instrument_id)
-        except InstrumentDoesNotExistError:
-            print('Error: no instrument with this id exists', file=sys.stderr)
-            exit(1)
+        if instrument_id is not None:
+            try:
+                instrument = get_instrument(instrument_id)
+            except InstrumentDoesNotExistError:
+                print('Error: no instrument with this id exists', file=sys.stderr)
+                exit(1)
         with open(schema_file_name, 'r', encoding='utf-8') as schema_file:
             schema = json.load(schema_file)
         try:
@@ -48,7 +52,7 @@ def main(arguments):
             print('Error: invalid schema: {}'.format(str(e)), file=sys.stderr)
             exit(1)
         action = create_action(
-            instrument_id=instrument.id,
+            instrument_id=instrument_id,
             action_type=action_type,
             name=name,
             description=description,
