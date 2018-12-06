2c703bc473afeb103385ae221d7eba2c5cac4ae2
Florian Rhiem <f.rhiem@fz-juelich.de>
2018-12-06 14:06:10 +0100

    Use logging module for migrations

--- sampledb/models/migrations/__init__.py
+++ sampledb/models/migrations/__init__.py
@@ -3,23 +3,26 @@
 
 """
 
+import logging
+
 from .utils import find_migrations, should_skip_by_index, update_migration_index
 
 
 def run(db):
+    logger = logging.getLogger('sampledb.migrations')
     for index, name, function in find_migrations():
-        print('Migration #{} "{}":'.format(index, name))
+        logger.info('Migration #{} "{}":'.format(index, name))
 
         # Skip migration by migration index
         if should_skip_by_index(db, index):
-            print("Skipped (index).")
+            logger.info("Skipped (index).")
             continue
 
         # Perform migration
         if function(db):
-            print("Done.")
+            logger.info("Done.")
         else:
-            print("Skipped (condition).")
+            logger.info("Skipped (condition).")
 
         # Update migration index to skip this migration by index in the future
         update_migration_index(db, index)
