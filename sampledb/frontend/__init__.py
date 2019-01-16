b508ebd32442b4c69da389b84170f42e4b4683c7
Florian Rhiem <f.rhiem@fz-juelich.de>
2019-01-16 09:15:29 +0100

    Added healthcheck to Dockerfile

--- sampledb/frontend/__init__.py
+++ sampledb/frontend/__init__.py
@@ -18,6 +18,7 @@ from . import locations
 from . import objects
 from . import objects_forms
 from . import projects
+from . import status
 from . import users
 from . import users_forms
 from . import errors
---
+++ sampledb/frontend/status.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+"""
+Status route to allow health checks.
+"""
+
+import flask
+
+
+from . import frontend
+from .. import db
+
+
+@frontend.route('/status/')
+def status():
+    status_info = {
+        'flask_status': True
+    }
+    try:
+        db.engine.execute('select 1').fetchone()
+        status_info['postgres_status'] = True
+    except Exception:
+        status_info['postgres_status'] = False
+    if all(status_info.values()):
+        status_code = 200
+    else:
+        status_code = 500
+    return flask.jsonify(status_info), status_code
