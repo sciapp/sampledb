50e78c2e4c220751437ae99746f7abdb8e28e34d
Florian Rhiem <f.rhiem@fz-juelich.de>
2019-02-20 08:51:04 +0100

    Removed user activity log from frontend

--- tests/frontend/test_user_log.py
+++ tests/frontend/test_user_log.py
@@ -37,7 +37,7 @@ def other_user(flask_server):
     return user
 
 
-def test_get_current_user_log(flask_server, user):
+def test_get_current_user_log_redirect(flask_server, user):
     session = requests.session()
     assert session.get(flask_server.base_url + 'users/{}/autologin'.format(user.id)).status_code == 200
     r = session.get(flask_server.base_url + 'users/me/activity')
@@ -46,32 +46,10 @@ def test_get_current_user_log(flask_server, user):
     assert r.status_code == 302
     assert 'Location' in r.headers
     url = r.headers['Location']
-    assert url == flask_server.base_url + 'users/{}/activity'.format(user.id)
+    assert url == flask_server.base_url + 'users/{}'.format(user.id)
 
 
-def test_get_user_log_empty(flask_server, user):
-    session = requests.session()
-    assert session.get(flask_server.base_url + 'users/{}/autologin'.format(user.id)).status_code == 200
-    r = session.get(flask_server.base_url + 'users/me/activity')
-    assert r.status_code == 200
-    document = BeautifulSoup(r.content, 'html.parser')
-    assert len(document.findAll('ol', {'class': 'user-activity-log'})) == 1
-    assert len(document.findAll('ol', {'class': 'user-activity-log'})[0].findAll('li')) == 0
-
-
-def test_get_user_log(flask_server, user):
-    session = requests.session()
-    assert session.get(flask_server.base_url + 'users/{}/autologin'.format(user.id)).status_code == 200
-    with flask_server.app.app_context():
-        sampledb.logic.user_log.create_object(user_id=user.id, object_id=0)
-    r = session.get(flask_server.base_url + 'users/me/activity')
-    assert r.status_code == 200
-    document = BeautifulSoup(r.content, 'html.parser')
-    assert len(document.findAll('ol', {'class': 'user-activity-log'})) == 1
-    assert len(document.findAll('ol', {'class': 'user-activity-log'})[0].findAll('li')) == 1
-
-
-def test_get_other_user_log(flask_server, user, other_user):
+def test_get_other_user_log_redirect(flask_server, user, other_user):
     session = requests.session()
     assert session.get(flask_server.base_url + 'users/{}/autologin'.format(user.id)).status_code == 200
     assert session.get(flask_server.base_url + 'users/{}/activity'.format(other_user.id)).status_code == 200
