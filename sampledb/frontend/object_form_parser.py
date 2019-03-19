afb6cb11d37b4173ba015ad2046aa12d44db34bf
Florian Rhiem <f.rhiem@fz-juelich.de>
2019-03-19 08:52:51 +0100

    Added support for dropdowns in tables

--- sampledb/frontend/object_form_parser.py
+++ sampledb/frontend/object_form_parser.py
@@ -57,6 +57,8 @@ def parse_text_form_data(form_data, schema, id_prefix, errors, required=False):
     if keys != [id_prefix + '__text']:
         raise ValueError('invalid text form data')
     text = form_data.get(id_prefix + '__text', [''])[0]
+    if not text and not required:
+        return None
     data = {
         '_type': 'text',
         'text': str(text)
