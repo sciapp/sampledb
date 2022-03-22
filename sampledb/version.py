1b7b599dece10e92f0d36edb2cc537ca085bac6a
Florian Rhiem <f.rhiem@fz-juelich.de>
2022-03-22 13:09:18 +0100

    Use git describe result in version when possible

--- sampledb/version.py
+++ sampledb/version.py
@@ -3,6 +3,12 @@
 This file should only contain the definition of the __version__ variable.
 
 It may be read by multiple other files, e.g. setup.py and the status page.
+
+The SAMPLEDB_VERSION environment variable can be used to override the static
+value. This is done when building the Docker image as part of the GitLab CI
+pipeline to provide the current version as returned by git describe.
 """
 
-__version__ = '0.20.0'
+import os
+
+__version__ = os.environ.get('SAMPLEDB_VERSION') or '0.20.0'
