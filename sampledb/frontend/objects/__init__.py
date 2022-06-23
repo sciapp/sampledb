b4e2e5c11fe856b34c49612131d6c75fe916882c
Florian Rhiem <f.rhiem@fz-juelich.de>
2022-06-23 08:28:49 +0200

    Extract file related routes from sampledb/frontend/objects/objects.py

--- sampledb/frontend/objects/__init__.py
+++ sampledb/frontend/objects/__init__.py
@@ -1,10 +1,12 @@
 from . import objects
+from . import files
 from . import forms
 from . import object_form_parser
 
 
 __all__ = [
     'objects',
+    'files',
     'forms',
     'object_form_parser',
 ]
