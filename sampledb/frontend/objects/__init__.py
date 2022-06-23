45d170a4502b352158be224686ae4d5cde956537
Florian Rhiem <f.rhiem@fz-juelich.de>
2022-06-23 12:16:45 +0200

    Extract routes for viewing and editing and object from sampledb/frontend/objects/objects.py

--- sampledb/frontend/objects/__init__.py
+++ sampledb/frontend/objects/__init__.py
@@ -1,20 +1,22 @@
-from . import objects
 from . import files
 from . import forms
+from . import objects
 from . import object_form
 from . import object_form_parser
 from . import permissions
 from . import search
 from . import versions
+from . import view
 
 
 __all__ = [
-    'objects',
     'files',
     'forms',
+    'objects',
     'object_form',
     'object_form_parser',
     'permissions',
     'search',
     'versions',
+    'view',
 ]
