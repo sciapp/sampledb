858bdfacf16e470f0b688eebb888dbeec43123a4
Florian Rhiem <f.rhiem@fz-juelich.de>
2022-06-23 11:22:59 +0200

    Extract functions related to the object form from sampledb/frontend/objects/objects.py

--- sampledb/frontend/objects/__init__.py
+++ sampledb/frontend/objects/__init__.py
@@ -1,6 +1,7 @@
 from . import objects
 from . import files
 from . import forms
+from . import object_form
 from . import object_form_parser
 from . import permissions
 from . import search
@@ -11,6 +12,7 @@ __all__ = [
     'objects',
     'files',
     'forms',
+    'object_form',
     'object_form_parser',
     'permissions',
     'search',
