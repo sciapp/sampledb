e6d8328ea4b6a15987b9013fe0119c31d4cc07d9
Florian Rhiem <f.rhiem@fz-juelich.de>
2022-09-30 16:14:19 +0200

    Extract action type functions from sampledb.logic.federation

--- sampledb/logic/federation/__init__.py
+++ sampledb/logic/federation/__init__.py
@@ -11,7 +11,6 @@ import requests
 import flask
 
 from ..action_translations import set_action_translation, get_action_translations_for_action
-from ..action_type_translations import get_action_type_translations_for_action_type, set_action_type_translation
 from ..files import create_fed_file, get_file, get_files_for_object, hide_file, File
 from ..languages import get_languages, get_language_by_lang_code, get_language
 from ..markdown_images import find_referenced_markdown_images, get_markdown_image
@@ -19,7 +18,7 @@ from ..object_permissions import set_user_object_permissions, set_group_object_p
 from ..schemas import validate_schema, validate
 from ... import db
 from .. import errors, fed_logs, languages, markdown_to_html
-from ..actions import get_action, create_action_type, get_action_type, update_action_type, create_action, get_mutable_action
+from ..actions import get_action, create_action, get_mutable_action
 from ..component_authentication import get_own_authentication
 from ..comments import get_comment, get_comments_for_object, create_comment, update_comment
 from ..components import get_component_by_uuid, get_component
@@ -29,7 +28,7 @@ from ..locations import create_fed_assignment, get_fed_object_location_assignmen
 from ..objects import get_fed_object, get_object, update_object_version, insert_fed_object_version, get_object_versions
 from ..projects import get_project
 from ..users import get_user
-from ...models import Permissions, ComponentAuthenticationType, Component, ActionType, MarkdownImage
+from ...models import Permissions, ComponentAuthenticationType, Component, MarkdownImage
 from ...models.file_log import FileLogEntry, FileLogEntryType
 
 from .utils import _get_id, _get_uuid, _get_bool, _get_str, _get_dict, _get_list, _get_utc_datetime, _get_translation, _get_permissions
@@ -37,6 +36,7 @@ from .components import _get_or_create_component_id
 from .users import import_user, parse_import_user, parse_user, _parse_user_ref, _get_or_create_user_id, shared_user_preprocessor
 from .location_types import import_location_type, parse_import_location_type, parse_location_type, _parse_location_type_ref, _get_or_create_location_type_id, shared_location_type_preprocessor
 from .instruments import import_instrument, parse_instrument, parse_import_instrument, _parse_instrument_ref, _get_or_create_instrument_id, shared_instrument_preprocessor
+from .action_types import import_action_type, parse_action_type, parse_import_action_type, _parse_action_type_ref, _get_or_create_action_type_id, shared_action_type_preprocessor
 
 
 PROTOCOL_VERSION_MAJOR = 0
@@ -269,66 +269,6 @@ def import_markdown_image(markdown_image_data, component):
         db.session.commit()
 
 
-def import_action_type(action_type_data, component):
-    component_id = _get_or_create_component_id(action_type_data['component_uuid'])
-
-    try:
-        action_type = get_action_type(action_type_data['fed_id'], component_id)
-
-        if action_type.admin_only != action_type_data['admin_only'] or action_type.enable_labels != action_type_data['enable_labels'] or action_type.enable_files != action_type_data['enable_files'] or action_type.enable_locations != action_type_data['enable_locations'] or action_type.enable_publications != action_type_data['enable_publications'] or action_type.enable_comments != action_type_data['enable_comments'] or action_type.enable_activity_log != action_type_data['enable_activity_log'] or action_type.enable_related_objects != action_type_data['enable_related_objects'] or action_type.enable_project_link != action_type_data['enable_project_link'] or action_type.disable_create_objects != action_type_data['disable_create_objects'] or action_type.is_template != action_type_data['is_template']:
-            update_action_type(
-                action_type_id=action_type.id,
-                admin_only=action_type_data['admin_only'],
-                show_on_frontpage=False,
-                show_in_navbar=False,
-                enable_labels=action_type_data['enable_labels'],
-                enable_files=action_type_data['enable_files'],
-                enable_locations=action_type_data['enable_locations'],
-                enable_publications=action_type_data['enable_publications'],
-                enable_comments=action_type_data['enable_comments'],
-                enable_activity_log=action_type_data['enable_activity_log'],
-                enable_related_objects=action_type_data['enable_related_objects'],
-                enable_project_link=action_type_data['enable_project_link'],
-                disable_create_objects=action_type_data['disable_create_objects'],
-                is_template=action_type_data['is_template'],
-                scicat_export_type=None
-            )
-            fed_logs.update_action_type(action_type.id, component.id)
-    except errors.ActionTypeDoesNotExistError:
-        action_type = create_action_type(
-            fed_id=action_type_data['fed_id'],
-            component_id=component_id,
-            admin_only=action_type_data['admin_only'],
-            show_on_frontpage=False,
-            show_in_navbar=False,
-            enable_labels=action_type_data['enable_labels'],
-            enable_files=action_type_data['enable_files'],
-            enable_locations=action_type_data['enable_locations'],
-            enable_publications=action_type_data['enable_publications'],
-            enable_comments=action_type_data['enable_comments'],
-            enable_activity_log=action_type_data['enable_activity_log'],
-            enable_related_objects=action_type_data['enable_related_objects'],
-            enable_project_link=action_type_data['enable_project_link'],
-            disable_create_objects=action_type_data['disable_create_objects'],
-            is_template=action_type_data['is_template'],
-            scicat_export_type=None
-        )
-        fed_logs.import_action_type(action_type.id, component.id)
-
-    for action_type_translation in action_type_data['translations']:
-        set_action_type_translation(
-            action_type_id=action_type.id,
-            language_id=action_type_translation['language_id'],
-            name=action_type_translation['name'],
-            description=action_type_translation['description'],
-            object_name=action_type_translation['object_name'],
-            object_name_plural=action_type_translation['object_name_plural'],
-            view_text=action_type_translation['view_text'],
-            perform_text=action_type_translation['perform_text']
-        )
-    return action_type
-
-
 def import_object(object_data, component):
     action_id = _get_or_create_action_id(object_data['action'])
     for version in object_data['versions']:
@@ -468,35 +408,6 @@ def import_object_location_assignment(assignment_data, object, component):
         db.session.commit()
         fed_logs.update_object_location_assignment(assignment.id, component.id)
     return assignment
-
-
-def _get_or_create_action_type_id(action_type_data):
-    if action_type_data is None:
-        return None
-    component_id = _get_or_create_component_id(action_type_data['component_uuid'])
-    try:
-        action_type = get_action_type(action_type_data['action_type_id'], component_id)
-    except errors.ActionTypeDoesNotExistError:
-        action_type = create_action_type(
-            admin_only=True,
-            show_on_frontpage=False,
-            show_in_navbar=False,
-            enable_labels=False,
-            enable_files=False,
-            enable_locations=False,
-            enable_publications=False,
-            enable_comments=False,
-            enable_activity_log=False,
-            enable_related_objects=False,
-            enable_project_link=False,
-            disable_create_objects=False,
-            is_template=False,
-            fed_id=action_type_data['action_type_id'],
-            component_id=component_id,
-            scicat_export_type=None
-        )
-        fed_logs.create_ref_action_type(action_type.id, component_id)
-    return action_type.id
 
 
 def _get_or_create_action_id(action_data):
@@ -686,13 +597,6 @@ def _parse_action_ref(action_data):
     return {'action_id': action_id, 'component_uuid': component_uuid}
 
 
-def _parse_action_type_ref(action_type_data):
-    action_type_id = _get_id(action_type_data.get('action_type_id'), special_values=[ActionType.SAMPLE_CREATION, ActionType.MEASUREMENT, ActionType.SIMULATION, ActionType.TEMPLATE])
-    component_uuid = _get_uuid(action_type_data.get('component_uuid'))
-
-    return {'action_type_id': action_type_id, 'component_uuid': component_uuid}
-
-
 def _parse_schema(schema, path=[]):
     if schema is None:
         return
@@ -791,54 +695,6 @@ def parse_action(action_data):
                 'name': _get_str(translation.get('name')),
                 'description': _get_str(translation.get('description')),
                 'short_description': _get_str(translation.get('short_description'))
-            })
-    return result
-
-
-def parse_action_type(action_type_data):
-    fed_id = _get_id(action_type_data.get('action_type_id'), special_values=[ActionType.SAMPLE_CREATION, ActionType.MEASUREMENT, ActionType.SIMULATION, ActionType.TEMPLATE])
-    uuid = _get_uuid(action_type_data.get('component_uuid'))
-    if uuid == flask.current_app.config['FEDERATION_UUID']:
-        # do not accept updates for own data
-        raise errors.InvalidDataExportError('Invalid update for local action type {}'.format(fed_id))
-    result = {
-        'fed_id': fed_id,
-        'component_uuid': uuid,
-        'admin_only': _get_bool(action_type_data.get('admin_only'), default=True),
-        'enable_labels': _get_bool(action_type_data.get('enable_labels'), default=False),
-        'enable_files': _get_bool(action_type_data.get('enable_files'), default=False),
-        'enable_locations': _get_bool(action_type_data.get('enable_locations'), default=False),
-        'enable_publications': _get_bool(action_type_data.get('enable_publications'), default=False),
-        'enable_comments': _get_bool(action_type_data.get('enable_comments'), default=False),
-        'enable_activity_log': _get_bool(action_type_data.get('enable_activity_log'), default=False),
-        'enable_related_objects': _get_bool(action_type_data.get('enable_related_objects'), default=False),
-        'enable_project_link': _get_bool(action_type_data.get('enable_project_link'), default=False),
-        'disable_create_objects': _get_bool(action_type_data.get('disable_create_objects'), default=False),
-        'is_template': _get_bool(action_type_data.get('is_template'), default=False),
-        'translations': []
-    }
-
-    allowed_language_ids = [language.id for language in get_languages(only_enabled_for_input=False)]
-
-    translation_data = _get_dict(action_type_data.get('translations'))
-    if translation_data is not None:
-        for lang_code, translation in translation_data.items():
-            try:
-                language = get_language_by_lang_code(lang_code.lower())
-            except errors.LanguageDoesNotExistError:
-                continue
-            language_id = language.id
-            if language_id not in allowed_language_ids:
-                continue
-
-            result['translations'].append({
-                'language_id': language_id,
-                'name': _get_str(translation.get('name')),
-                'description': _get_str(translation.get('description')),
-                'object_name': _get_str(translation.get('object_name')),
-                'object_name_plural': _get_str(translation.get('object_name_plural')),
-                'view_text': _get_str(translation.get('view_text')),
-                'perform_text': _get_str(translation.get('perform_text'))
             })
     return result
 
@@ -1519,43 +1375,6 @@ def shared_action_preprocessor(action_id: int, _component: Component, refs: list
     }
 
 
-def shared_action_type_preprocessor(action_type_id, _component, _refs, _markdown_images):
-    action_type = get_action_type(action_type_id)
-    if action_type.component_id is not None:
-        return None
-    translations_data = {}
-    try:
-        translations = get_action_type_translations_for_action_type(action_type.id)
-        for translation in translations:
-            lang_code = get_language(translation.language_id).lang_code
-            translations_data[lang_code] = {
-                'name': translation.name,
-                'description': translation.description,
-                'object_name': translation.object_name,
-                'object_name_plural': translation.object_name_plural,
-                'view_text': translation.view_text,
-                'perform_text': translation.perform_text
-            }
-    except errors.ActionTypeTranslationDoesNotExistError:
-        translations_data = None
-    return {
-        'action_type_id': action_type.id if action_type.component_id is None else action_type.fed_id,
-        'component_uuid': flask.current_app.config['FEDERATION_UUID'] if action_type.component_id is None else get_component(action_type.component_id).uuid,
-        'admin_only': action_type.admin_only,
-        'enable_labels': action_type.enable_labels,
-        'enable_files': action_type.enable_files,
-        'enable_locations': action_type.enable_locations,
-        'enable_publications': action_type.enable_publications,
-        'enable_comments': action_type.enable_comments,
-        'enable_activity_log': action_type.enable_activity_log,
-        'enable_related_objects': action_type.enable_related_objects,
-        'enable_project_link': action_type.enable_project_link,
-        'disable_create_objects': action_type.disable_create_objects,
-        'is_template': action_type.is_template,
-        'translations': translations_data
-    }
-
-
 def shared_location_preprocessor(location_id: int, _component: Component, refs: list, _markdown_images):
     location = get_location(location_id)
     if location.component is not None:
@@ -1629,10 +1448,6 @@ def parse_import_location(location_data, component):
 
 def parse_import_action(action_data, component):
     return import_action(parse_action(action_data), component)
-
-
-def parse_import_action_type(action_type_data, component):
-    return import_action_type(parse_action_type(action_type_data), component)
 
 
 def parse_import_comment(comment_data, object, component):
---
+++ sampledb/logic/federation/action_types.py
@@ -0,0 +1,194 @@
+import flask
+
+from .components import _get_or_create_component_id
+from .utils import _get_id, _get_uuid, _get_bool, _get_str, _get_dict
+from ..languages import get_languages, get_language, get_language_by_lang_code
+from ..actions import get_action_type, update_action_type, create_action_type, ActionType
+from ..action_type_translations import set_action_type_translation, get_action_type_translations_for_action_type
+from ..components import get_component
+from .. import errors, fed_logs
+
+
+def import_action_type(action_type_data, component):
+    component_id = _get_or_create_component_id(action_type_data['component_uuid'])
+
+    try:
+        action_type = get_action_type(action_type_data['fed_id'], component_id)
+
+        if action_type.admin_only != action_type_data['admin_only'] or action_type.enable_labels != action_type_data['enable_labels'] or action_type.enable_files != action_type_data['enable_files'] or action_type.enable_locations != action_type_data['enable_locations'] or action_type.enable_publications != action_type_data['enable_publications'] or action_type.enable_comments != action_type_data['enable_comments'] or action_type.enable_activity_log != action_type_data['enable_activity_log'] or action_type.enable_related_objects != action_type_data['enable_related_objects'] or action_type.enable_project_link != action_type_data['enable_project_link'] or action_type.disable_create_objects != action_type_data['disable_create_objects'] or action_type.is_template != action_type_data['is_template']:
+            update_action_type(
+                action_type_id=action_type.id,
+                admin_only=action_type_data['admin_only'],
+                show_on_frontpage=False,
+                show_in_navbar=False,
+                enable_labels=action_type_data['enable_labels'],
+                enable_files=action_type_data['enable_files'],
+                enable_locations=action_type_data['enable_locations'],
+                enable_publications=action_type_data['enable_publications'],
+                enable_comments=action_type_data['enable_comments'],
+                enable_activity_log=action_type_data['enable_activity_log'],
+                enable_related_objects=action_type_data['enable_related_objects'],
+                enable_project_link=action_type_data['enable_project_link'],
+                disable_create_objects=action_type_data['disable_create_objects'],
+                is_template=action_type_data['is_template'],
+                scicat_export_type=None
+            )
+            fed_logs.update_action_type(action_type.id, component.id)
+    except errors.ActionTypeDoesNotExistError:
+        action_type = create_action_type(
+            fed_id=action_type_data['fed_id'],
+            component_id=component_id,
+            admin_only=action_type_data['admin_only'],
+            show_on_frontpage=False,
+            show_in_navbar=False,
+            enable_labels=action_type_data['enable_labels'],
+            enable_files=action_type_data['enable_files'],
+            enable_locations=action_type_data['enable_locations'],
+            enable_publications=action_type_data['enable_publications'],
+            enable_comments=action_type_data['enable_comments'],
+            enable_activity_log=action_type_data['enable_activity_log'],
+            enable_related_objects=action_type_data['enable_related_objects'],
+            enable_project_link=action_type_data['enable_project_link'],
+            disable_create_objects=action_type_data['disable_create_objects'],
+            is_template=action_type_data['is_template'],
+            scicat_export_type=None
+        )
+        fed_logs.import_action_type(action_type.id, component.id)
+
+    for action_type_translation in action_type_data['translations']:
+        set_action_type_translation(
+            action_type_id=action_type.id,
+            language_id=action_type_translation['language_id'],
+            name=action_type_translation['name'],
+            description=action_type_translation['description'],
+            object_name=action_type_translation['object_name'],
+            object_name_plural=action_type_translation['object_name_plural'],
+            view_text=action_type_translation['view_text'],
+            perform_text=action_type_translation['perform_text']
+        )
+    return action_type
+
+
+def parse_action_type(action_type_data):
+    fed_id = _get_id(action_type_data.get('action_type_id'), special_values=[ActionType.SAMPLE_CREATION, ActionType.MEASUREMENT, ActionType.SIMULATION, ActionType.TEMPLATE])
+    uuid = _get_uuid(action_type_data.get('component_uuid'))
+    if uuid == flask.current_app.config['FEDERATION_UUID']:
+        # do not accept updates for own data
+        raise errors.InvalidDataExportError('Invalid update for local action type {}'.format(fed_id))
+    result = {
+        'fed_id': fed_id,
+        'component_uuid': uuid,
+        'admin_only': _get_bool(action_type_data.get('admin_only'), default=True),
+        'enable_labels': _get_bool(action_type_data.get('enable_labels'), default=False),
+        'enable_files': _get_bool(action_type_data.get('enable_files'), default=False),
+        'enable_locations': _get_bool(action_type_data.get('enable_locations'), default=False),
+        'enable_publications': _get_bool(action_type_data.get('enable_publications'), default=False),
+        'enable_comments': _get_bool(action_type_data.get('enable_comments'), default=False),
+        'enable_activity_log': _get_bool(action_type_data.get('enable_activity_log'), default=False),
+        'enable_related_objects': _get_bool(action_type_data.get('enable_related_objects'), default=False),
+        'enable_project_link': _get_bool(action_type_data.get('enable_project_link'), default=False),
+        'disable_create_objects': _get_bool(action_type_data.get('disable_create_objects'), default=False),
+        'is_template': _get_bool(action_type_data.get('is_template'), default=False),
+        'translations': []
+    }
+
+    allowed_language_ids = [language.id for language in get_languages(only_enabled_for_input=False)]
+
+    translation_data = _get_dict(action_type_data.get('translations'))
+    if translation_data is not None:
+        for lang_code, translation in translation_data.items():
+            try:
+                language = get_language_by_lang_code(lang_code.lower())
+            except errors.LanguageDoesNotExistError:
+                continue
+            language_id = language.id
+            if language_id not in allowed_language_ids:
+                continue
+
+            result['translations'].append({
+                'language_id': language_id,
+                'name': _get_str(translation.get('name')),
+                'description': _get_str(translation.get('description')),
+                'object_name': _get_str(translation.get('object_name')),
+                'object_name_plural': _get_str(translation.get('object_name_plural')),
+                'view_text': _get_str(translation.get('view_text')),
+                'perform_text': _get_str(translation.get('perform_text'))
+            })
+    return result
+
+
+def parse_import_action_type(action_type_data, component):
+    return import_action_type(parse_action_type(action_type_data), component)
+
+
+def _parse_action_type_ref(action_type_data):
+    action_type_id = _get_id(action_type_data.get('action_type_id'), special_values=[ActionType.SAMPLE_CREATION, ActionType.MEASUREMENT, ActionType.SIMULATION, ActionType.TEMPLATE])
+    component_uuid = _get_uuid(action_type_data.get('component_uuid'))
+
+    return {'action_type_id': action_type_id, 'component_uuid': component_uuid}
+
+
+def _get_or_create_action_type_id(action_type_data):
+    if action_type_data is None:
+        return None
+    component_id = _get_or_create_component_id(action_type_data['component_uuid'])
+    try:
+        action_type = get_action_type(action_type_data['action_type_id'], component_id)
+    except errors.ActionTypeDoesNotExistError:
+        action_type = create_action_type(
+            admin_only=True,
+            show_on_frontpage=False,
+            show_in_navbar=False,
+            enable_labels=False,
+            enable_files=False,
+            enable_locations=False,
+            enable_publications=False,
+            enable_comments=False,
+            enable_activity_log=False,
+            enable_related_objects=False,
+            enable_project_link=False,
+            disable_create_objects=False,
+            is_template=False,
+            fed_id=action_type_data['action_type_id'],
+            component_id=component_id,
+            scicat_export_type=None
+        )
+        fed_logs.create_ref_action_type(action_type.id, component_id)
+    return action_type.id
+
+
+def shared_action_type_preprocessor(action_type_id, _component, _refs, _markdown_images):
+    action_type = get_action_type(action_type_id)
+    if action_type.component_id is not None:
+        return None
+    translations_data = {}
+    try:
+        translations = get_action_type_translations_for_action_type(action_type.id)
+        for translation in translations:
+            lang_code = get_language(translation.language_id).lang_code
+            translations_data[lang_code] = {
+                'name': translation.name,
+                'description': translation.description,
+                'object_name': translation.object_name,
+                'object_name_plural': translation.object_name_plural,
+                'view_text': translation.view_text,
+                'perform_text': translation.perform_text
+            }
+    except errors.ActionTypeTranslationDoesNotExistError:
+        translations_data = None
+    return {
+        'action_type_id': action_type.id if action_type.component_id is None else action_type.fed_id,
+        'component_uuid': flask.current_app.config['FEDERATION_UUID'] if action_type.component_id is None else get_component(action_type.component_id).uuid,
+        'admin_only': action_type.admin_only,
+        'enable_labels': action_type.enable_labels,
+        'enable_files': action_type.enable_files,
+        'enable_locations': action_type.enable_locations,
+        'enable_publications': action_type.enable_publications,
+        'enable_comments': action_type.enable_comments,
+        'enable_activity_log': action_type.enable_activity_log,
+        'enable_related_objects': action_type.enable_related_objects,
+        'enable_project_link': action_type.enable_project_link,
+        'disable_create_objects': action_type.disable_create_objects,
+        'is_template': action_type.is_template,
+        'translations': translations_data
+    }
