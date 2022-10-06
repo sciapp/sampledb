37cf914523e604d91e5592516b108e754dd0145a
Florian Rhiem <f.rhiem@fz-juelich.de>
2022-10-06 11:02:39 +0200

    Use dataclass for sampledb.logic.instrument_translations.InstrumentTranslation

--- sampledb/logic/instrument_translations.py
+++ sampledb/logic/instrument_translations.py
@@ -6,7 +6,7 @@ Instrument translations complement instruments.
 Translations contain all linguistic elements of instruments such as names.
 """
 
-import collections
+import dataclasses
 import typing
 from flask_babel import _
 
@@ -16,25 +16,18 @@ from .. import models
 from . import errors, languages, instruments
 
 
-class InstrumentTranslation(collections.namedtuple(
-    'InstrumentTranslation', ['instrument_id', 'language_id', 'name', 'description', 'short_description', 'notes']
-)):
+@dataclasses.dataclass(frozen=True)
+class InstrumentTranslation:
     """
     This class provides an immutable wrapper around models.instrument_translations.InstrumentTranslation.
     """
-
-    def __new__(
-            cls,
-            instrument_id: int,
-            language_id: int,
-            name: str,
-            description: str,
-            short_description: str,
-            notes: str
-    ) -> 'InstrumentTranslation':
-        self = super(InstrumentTranslation, cls).__new__(cls, instrument_id, language_id, name, description, short_description, notes)
-        self._language = None
-        return self
+    instrument_id: int
+    language_id: int
+    name: str
+    description: str
+    short_description: str
+    notes: str
+    _language_cache: typing.List[languages.Language] = dataclasses.field(default_factory=list, kw_only=True, repr=False, compare=False)
 
     @classmethod
     def from_database(cls, instrument_translation: models.InstrumentTranslation) -> 'InstrumentTranslation':
@@ -49,10 +42,9 @@ class InstrumentTranslation(collections.namedtuple(
 
     @property
     def language(self) -> Language:
-        self._language: typing.Optional[Language]
-        if self._language is None:
-            self._language = languages.get_language(self.language_id)
-        return self._language
+        if not self._language_cache:
+            self._language_cache.append(languages.get_language(self.language_id))
+        return self._language_cache[0]
 
 
 def set_instrument_translation(
