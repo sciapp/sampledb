7db2b99e9167a1c409f82f09ec1c03175e384d9a
Jannik Streitberg <j.streitberg@fz-juelich.de>
2023-02-15 13:55:06 +0100

    Fix issue of label generation by modifying url directly

--- tests/frontend/test_status_codes.py
+++ tests/frontend/test_status_codes.py
@@ -371,7 +371,7 @@ def test_status_codes(flask_server, user):
         f'objects/{other_object_id}/files/': 200,
         f'objects/{other_object_id}/files/{file_id}': 404,
         f'objects/{other_object_id}/files/mobile_upload/{token}': 400,  # 400 because mobile upload requires valid token
-        f'objects/{other_object_id}/label': 200,
+        f'objects/{other_object_id}/label': 403,
         f'objects/{other_object_id}/permissions': 200,
         f'objects/{other_object_id}/scicat_export/': 302,
         f'objects/{other_object_id}/versions/': 200,
