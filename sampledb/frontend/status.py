a5188639434906cc708fbd000dfd79a89e2aabb9
Florian Rhiem <f.rhiem@fz-juelich.de>
2023-05-22 15:37:39 +0200

    Add federation UUID and service name to /status route

--- sampledb/frontend/status.py
+++ sampledb/frontend/status.py
@@ -27,4 +27,6 @@ def status() -> FlaskResponseT:
         status_code = 200
     else:
         status_code = 500
+    status_info['name'] = flask.current_app.config['SERVICE_NAME']
+    status_info['federation_uuid'] = flask.current_app.config['FEDERATION_UUID']
     return flask.jsonify(status_info), status_code
