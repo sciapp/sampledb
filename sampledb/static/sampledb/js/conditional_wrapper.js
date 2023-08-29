71c34895d2af04eaf491d5b1416387bf3090ebac
Florian Rhiem <f.rhiem@fz-juelich.de>
2023-08-29 08:33:23 +0200

    Fix condition replacements in tables

--- sampledb/static/sampledb/js/conditional_wrapper.js
+++ sampledb/static/sampledb/js/conditional_wrapper.js
@@ -45,10 +45,12 @@ function conditional_wrapper(parent_id_prefix, id_prefix, schema_conditions) {
       }
       let all_conditions_fulfilled = check_all_conditions_fulfilled(window.conditional_wrapper_conditions[id_prefix]);
       if (all_conditions_fulfilled) {
+        $(`[data-condition-wrapper-for="${id_prefix}"]`).removeClass('hidden');
         $(`[data-condition-wrapper-for="${id_prefix}"]`).show();
         $(`[data-condition-replacement-for="${id_prefix}"]`).hide();
       } else {
         $(`[data-condition-wrapper-for="${id_prefix}"]`).hide();
+        $(`[data-condition-replacement-for="${id_prefix}"]`).removeClass('hidden');
         $(`[data-condition-replacement-for="${id_prefix}"]`).show();
       }
       $(`[data-condition-wrapper-for="${id_prefix}"] input, [data-condition-wrapper-for="${id_prefix}"] textarea, [data-condition-wrapper-for="${id_prefix}"] select`).prop('disabled', !all_conditions_fulfilled).attr('data-sampledb-disabled-by-condition', !all_conditions_fulfilled).trigger('conditions_state_changed.sampledb');
