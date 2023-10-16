5424d69537aca081dabc3bbe5278e49b83754841
Florian Rhiem <f.rhiem@fz-juelich.de>
2023-10-16 11:05:09 +0200

    Clean up sampledb/static/sampledb/js/markdown_image_viewer.js and convert it into a module

--- sampledb/static/sampledb/js/markdown_image_viewer.js
+++ sampledb/static/sampledb/js/markdown_image_viewer.js
@@ -1,24 +1,29 @@
-$(function() {
-  $('.action-user-content img, .instrument-user-content img, .object-user-content img').each(function(_, img) {
-    $(img).click(function() {
-      console.log(img);
-      var preview = $('<span class="fullscreen-image-preview">\
-          <span class="close-fullscreen-image-preview"><i class="fa fa-close fa-fw"></i></span>\
-          <a href="#">\
-            <span class="download-fullscreen-image-preview"><i class="fa fa-download fa-fw"></i></span>\
-          </a>\
-          <img src="" alt="Fullscreen Image Preview">\
-        </span>');
+'use strict';
+/* eslint-env jquery */
+
+// Sets event handlers for showing previews of markdown images when clicking on them.
+$(function () {
+  $('.action-user-content img, .instrument-user-content img, .object-user-content img').each(function (_, img) {
+    $(img).click(function () {
+      const preview = $(
+        '<span class="fullscreen-image-preview">' +
+        '  <span class="close-fullscreen-image-preview"><i class="fa fa-close fa-fw"></i></span>' +
+        '  <a href="#">' +
+        '    <span class="download-fullscreen-image-preview"><i class="fa fa-download fa-fw"></i></span>' +
+        '  </a>' +
+        '  <img src="" alt="Fullscreen Image Preview">' +
+        '</span>'
+      );
       $(this).parent().append(preview);
       preview.find('img').attr('src', $(this).attr('src'));
       preview.find('a').attr('href', $(this).attr('src'));
-      preview.on('click', function(e) {
+      preview.on('click', function (e) {
         $(this).hide();
       });
-      preview.find('img').on('click', function(e) {
+      preview.find('img').on('click', function (e) {
         e.stopPropagation();
       });
-      preview.find('.download-fullscreen-image-preview').on('click', function(e) {
+      preview.find('.download-fullscreen-image-preview').on('click', function (e) {
         e.stopPropagation();
       });
       preview.show();
