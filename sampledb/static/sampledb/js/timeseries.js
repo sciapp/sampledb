ae81ba03cd4497ae4f797ee7bf084df1278da8da
Florian Rhiem <f.rhiem@fz-juelich.de>
2023-10-17 10:55:03 +0200

    Clean up sampledb/static/sampledb/js/timeseries.js and convert it into a module

--- sampledb/static/sampledb/js/timeseries.js
+++ sampledb/static/sampledb/js/timeseries.js
@@ -1,118 +1,122 @@
+'use strict';
+/* eslint-env jquery */
+/* globals Plotly */
+
 window.all_timeseries_data = [];
-$(function() {
-  $('.timeseries-container').each(function(index, element) {
-    let timeseries_data = JSON.parse($(element).find('script[type="application/json"]')[0].textContent);
+$(function () {
+  $('.timeseries-container').each(function (index, element) {
+    const timeseriesData = JSON.parse($(element).find('script[type="application/json"]')[0].textContent);
     window.all_timeseries_data.push({
       id: $(element).data('sampledbTimeseriesId'),
       title: $(element).data('sampledbTimeseriesTitle'),
-      data: timeseries_data
+      data: timeseriesData
     });
-    $(element).find('.timeseries-chart-button').on('click', function() {
-      let current_id = $(this).closest('.timeseries-container').data('sampledbTimeseriesId');
-      let chart_modal = $('#timeseriesChartModal');
-      let chart_modal_body = chart_modal.find('.modal-body');
-      let chart_modal_title = chart_modal.find('.modal-title');
+    $(element).find('.timeseries-chart-button').on('click', function () {
+      const currentID = $(this).closest('.timeseries-container').data('sampledbTimeseriesId');
+      const chartModal = $('#timeseriesChartModal');
+      const chartModalBody = chartModal.find('.modal-body');
+      const chartModalTitle = chartModal.find('.modal-title');
       if (window.all_timeseries_data.length === 1) {
-        chart_modal_title.show();
-        chart_modal.find('.timeseries-picker-wrapper').hide();
+        chartModalTitle.show();
+        chartModal.find('.timeseries-picker-wrapper').hide();
       } else {
-        chart_modal_title.hide();
-        chart_modal.find('.timeseries-picker-wrapper').show();
+        chartModalTitle.hide();
+        chartModal.find('.timeseries-picker-wrapper').show();
       }
-      chart_modal_title.text(timeseries_data.title);
-      let chart_selectpicker = chart_modal.find('.modal-header select.selectpicker');
-      chart_selectpicker.html('');
-      let titles = [];
-      let has_duplicates = false;
+      chartModalTitle.text(timeseriesData.title);
+      const chartSelectpicker = chartModal.find('.modal-header select.selectpicker');
+      chartSelectpicker.html('');
+      const titles = [];
+      let hasDuplicates = false;
       for (let i = 0; i < window.all_timeseries_data.length; i++) {
-        let ts = window.all_timeseries_data[i];
+        const ts = window.all_timeseries_data[i];
         if (titles.includes(ts.title)) {
-          has_duplicates = true;
+          hasDuplicates = true;
         } else {
           titles.push(ts.title);
         }
       }
       for (let i = 0; i < window.all_timeseries_data.length; i++) {
-        let ts = window.all_timeseries_data[i];
+        const ts = window.all_timeseries_data[i];
         let name = ts.title;
-        if (has_duplicates) {
-          name +=' (' + ts.id.replaceAll('__', ' ➜ ') + ')';
+        if (hasDuplicates) {
+          name += ' (' + ts.id.replaceAll('__', ' ➜ ') + ')';
         }
-        let option = $('<option></option>');
+        const option = $('<option></option>');
         option.attr('value', ts.id);
         option.text(name);
-        chart_selectpicker.append(option);
+        chartSelectpicker.append(option);
       }
-      chart_selectpicker.selectpicker('refresh');
-      chart_selectpicker.selectpicker('val', current_id);
-      chart_selectpicker.on('changed.bs.select', function() {
-        let timeseries_ids = chart_selectpicker.val();
-        let titles = [];
-        let has_duplicates = false;
-        let rel_tick_suffix = null;
-        let abs_tick_suffix = null;
+      chartSelectpicker.selectpicker('refresh');
+      chartSelectpicker.selectpicker('val', currentID);
+      chartSelectpicker.on('changed.bs.select', function () {
+        const timeseriesIDs = chartSelectpicker.val();
+        const titles = [];
+        let hasDuplicates = false;
+        let relativeTickSuffix = null;
+        let absoluteTickSuffix = null;
         for (let i = 0; i < window.all_timeseries_data.length; i++) {
-          let ts = window.all_timeseries_data[i];
-          if (timeseries_ids.includes(ts.id)) {
+          const ts = window.all_timeseries_data[i];
+          if (timeseriesIDs.includes(ts.id)) {
             if (titles.includes(ts.title)) {
-              has_duplicates = true;
+              hasDuplicates = true;
             } else {
               titles.push(ts.title);
             }
             if (ts.data.relative_times.length > 0) {
-              if (rel_tick_suffix === null) {
-                rel_tick_suffix = ts.data.units;
-              } else if (rel_tick_suffix !== ts.data.units) {
-                rel_tick_suffix = '';
+              if (relativeTickSuffix === null) {
+                relativeTickSuffix = ts.data.units;
+              } else if (relativeTickSuffix !== ts.data.units) {
+                relativeTickSuffix = '';
               }
             } else {
-              if (abs_tick_suffix === null) {
-                abs_tick_suffix = ts.data.units;
-              } else if (abs_tick_suffix !== ts.data.units) {
-                abs_tick_suffix = '';
+              if (absoluteTickSuffix === null) {
+                absoluteTickSuffix = ts.data.units;
+              } else if (absoluteTickSuffix !== ts.data.units) {
+                absoluteTickSuffix = '';
               }
             }
           }
         }
-        let chart_data = [];
+        const chartData = [];
         let relative = false;
         let absolute = false;
-        for(let i = 0; i < window.all_timeseries_data.length; i++) {
-          let ts = window.all_timeseries_data[i];
-          if (timeseries_ids.includes(ts.id)){
+        for (let i = 0; i < window.all_timeseries_data.length; i++) {
+          const ts = window.all_timeseries_data[i];
+          if (timeseriesIDs.includes(ts.id)) {
             let name = ts.title;
-            if (has_duplicates) {
+            if (hasDuplicates) {
               name += ' (' + ts.id.replaceAll('__', ' ➜ ') + ')';
             }
             if (ts.data.relative_times.length > 0) {
               relative = true;
-              if (rel_tick_suffix === '' && ts.data.units.trim().length > 0) {
+              if (relativeTickSuffix === '' && ts.data.units.trim().length > 0) {
                 name += ' [' + ts.data.units.trim() + ']';
               }
-              chart_data.push({
+              chartData.push({
                 x: ts.data.relative_times,
                 y: ts.data.magnitudes,
                 type: 'scatter',
-                name: name
+                name
               });
             } else {
               absolute = true;
-              if (abs_tick_suffix === '' && ts.data.units.trim().length > 0) {
+              if (absoluteTickSuffix === '' && ts.data.units.trim().length > 0) {
                 name += ' [' + ts.data.units.trim() + ']';
               }
-              chart_data.push({
+              chartData.push({
                 x: ts.data.times_strings,
                 y: ts.data.magnitudes,
                 xaxis: 'x2',
                 yaxis: 'y2',
                 type: 'scatter',
-                name: name
+                name
               });
             }
           }
         }
-        if (chart_data.length > 0) {
-          let chart_layout = {
+        if (chartData.length > 0) {
+          const chartLayout = {
             margin: {
               l: 50,
               r: 50,
@@ -122,54 +126,54 @@ $(function() {
             },
             width: 868,
             legend: {
-              orientation: "h"
-            },
+              orientation: 'h'
+            }
           };
           if (relative && absolute) {
-            chart_layout.grid = {
+            chartLayout.grid = {
               rows: 2,
               columns: 1,
               pattern: 'independent'
-            }
-            chart_layout.height = 668;
+            };
+            chartLayout.height = 668;
           }
           if (relative) {
-            chart_layout.xaxis = {
+            chartLayout.xaxis = {
               ticksuffix: 's'
             };
-            chart_layout.yaxis = {
-              ticksuffix: rel_tick_suffix
+            chartLayout.yaxis = {
+              ticksuffix: relativeTickSuffix
             };
           }
           if (absolute) {
-            chart_layout.xaxis2 = {
+            chartLayout.xaxis2 = {
               ticksuffix: ''
             };
-            chart_layout.yaxis2 = {
-              ticksuffix: abs_tick_suffix
+            chartLayout.yaxis2 = {
+              ticksuffix: absoluteTickSuffix
             };
           }
-          chart_modal_body.html('<div></div>');
-          Plotly.newPlot(chart_modal_body.children()[0], chart_data, chart_layout);
+          chartModalBody.html('<div></div>');
+          Plotly.newPlot(chartModalBody.children()[0], chartData, chartLayout);
         } else {
-          chart_modal_body.html('<p class="text-center text-muted"></p>');
-          chart_modal_body.children().text(window.select_timeseries_text);
+          chartModalBody.html('<p class="text-center text-muted"></p>');
+          chartModalBody.children().text(window.select_timeseries_text);
         }
       }).trigger('changed.bs.select');
-      chart_modal.modal();
+      chartModal.modal();
     });
-    $(element).find('.timeseries-table-button').on('click', function() {
-      let table_modal = $('#timeseriesTableModal');
-      table_modal.find('.modal-title').text(timeseries_data.title);
-      let table = table_modal.find('table');
-      let tbody = table.find('tbody');
+    $(element).find('.timeseries-table-button').on('click', function () {
+      const tableModal = $('#timeseriesTableModal');
+      tableModal.find('.modal-title').text(timeseriesData.title);
+      const table = tableModal.find('table');
+      const tbody = table.find('tbody');
       tbody.html('');
-      for (let i = 0; i < timeseries_data.times_strings.length; i++) {
-          let tr = $('<tr/>').appendTo(tbody);
-          tr.append('<td>' + timeseries_data.times_strings[i] + '</td>');
-          tr.append('<td>' + timeseries_data.magnitude_strings[i] + timeseries_data.units + '</td>');
+      for (let i = 0; i < timeseriesData.times_strings.length; i++) {
+        const tr = $('<tr/>').appendTo(tbody);
+        tr.append('<td>' + timeseriesData.times_strings[i] + '</td>');
+        tr.append('<td>' + timeseriesData.magnitude_strings[i] + timeseriesData.units + '</td>');
       }
-      table_modal.modal();
+      tableModal.modal();
     });
   });
 });
