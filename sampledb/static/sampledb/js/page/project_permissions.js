2b8020dcf08bfbc7c9415d3740fc66b4c013c4cc
Florian Rhiem <f.rhiem@fz-juelich.de>
2023-11-08 14:15:30 +0100

    Fix static checks for project_permissions.js

--- sampledb/static/sampledb/js/page/project_permissions.js
+++ sampledb/static/sampledb/js/page/project_permissions.js
@@ -1,22 +1,26 @@
+'use strict';
+/* eslint-env jquery */
+
 $(function () {
-  $('#form-permissions').find('button[type=submit]').on('click', function() {
-    num_read_users = $('.permissions_user_read:checked').length;
-    num_write_users = $('.permissions_user_write:checked').length;
-    num_grant_users = $('.permissions_user_grant:checked').length;
-    num_read_groups = $('.permissions_group_read:checked').length;
-    num_write_groups = $('.permissions_group_write:checked').length;
-    num_grant_groups = $('.permissions_group_grant:checked').length;
-    if (num_grant_users === 0 && (num_read_users + num_write_users + num_read_groups + num_write_groups + num_grant_groups) !== 0) {
+  // show warnings or delete modal in special cases
+  $('#form-permissions').find('button[type=submit]').on('click', function () {
+    const numReadUsers = $('.permissions_user_read:checked').length;
+    const numWriteUsers = $('.permissions_user_write:checked').length;
+    const numGrantUsers = $('.permissions_user_grant:checked').length;
+    const numReadGroups = $('.permissions_group_read:checked').length;
+    const numWriteGroups = $('.permissions_group_write:checked').length;
+    const numGrantGroups = $('.permissions_group_grant:checked').length;
+    if (numGrantUsers === 0 && (numReadUsers + numWriteUsers + numReadGroups + numWriteGroups + numGrantGroups) !== 0) {
       $('#alert-no-grant-user').html(`<div class="alert alert-danger alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>${window.template_values.min_grant_user_warning}</div>`);
       return false;
     }
-    if (num_read_users + num_write_users + num_grant_users + num_read_groups + num_write_groups + num_grant_groups === 0) {
+    if (numReadUsers + numWriteUsers + numGrantUsers + numReadGroups + numWriteGroups + numGrantGroups === 0) {
       $('#deleteProjectModal').modal('show');
       return false;
     }
     return true;
   });
-  $('#button-delete-project').on('click', function() {
+  $('#button-delete-project').on('click', function () {
     $('#form-permissions').submit();
   });
-})
+});
