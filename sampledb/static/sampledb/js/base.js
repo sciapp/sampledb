cf01125f76c10567157f47e7fe2ddfbd4f2abfea
Florian Rhiem <f.rhiem@fz-juelich.de>
2023-11-08 10:21:17 +0100

    Fix static checks for base.js and add documentation

--- sampledb/static/sampledb/js/base.js
+++ sampledb/static/sampledb/js/base.js
@@ -1,60 +1,71 @@
-$(function() {
+'use strict';
+/* eslint-env jquery */
+/* globals Cookies, moment */
+
+$(function () {
   $('[data-toggle="tooltip"]').tooltip();
+
+  // update timezone for users who use the auto_tz setting
   if (window.template_values.current_user.is_authenticated) {
     if (window.template_values.current_user.settings.auto_tz) {
       const tz = moment.tz.guess(true);
       if (tz !== window.template_values.current_user.settings.timezone) {
         $.post(window.template_values.current_user.timezone_url, {
-          "timezone": tz,
-          "csrf_token": window.template_values.current_user.timezone_csrf_token
+          timezone: tz,
+          csrf_token: window.template_values.current_user.timezone_csrf_token
         });
       }
     }
   }
+
   // localize datetimes for anonymous users if the timezone has not been set in the config
   if (window.template_values.current_user.has_timezone) {
-    $('span[data-utc-datetime]').each(function(_, element) {
-        let utc_datetime_str = $(element).data('utcDatetime');
-        let utc_datetime = moment.utc(utc_datetime_str);
-        let local_datetime = utc_datetime.local();
-        let lang_code = window.template_values.current_user.language.lang_code;
-        let format = window.template_values.current_user.language.datetime_format_moment_output;
-        element.innerText = local_datetime.locale(lang_code).format(format);
+    $('span[data-utc-datetime]').each(function (_, element) {
+      const utcDatetimeStr = $(element).data('utcDatetime');
+      const utcDatetime = moment.utc(utcDatetimeStr);
+      const localDatetime = utcDatetime.local();
+      const langCode = window.template_values.current_user.language.lang_code;
+      const format = window.template_values.current_user.language.datetime_format_moment_output;
+      element.innerText = localDatetime.locale(langCode).format(format);
     });
   }
 
+  // handle scrolling for the sidebar navigation
   $(window).on('scroll', function () {
-    let active_a = null;
-    let scroll_offset = 50;
-    $('#sidebar > nav a').removeClass('active').each(function(_, a) {
+    let activeA = null;
+    const scrollOffset = 50;
+    $('#sidebar > nav a').removeClass('active').each(function (_, a) {
       a = $(a);
-      let href = a.attr('href');
-      if (href === "#top") {
+      const href = a.attr('href');
+      if (href === '#top') {
         return;
       }
-      let linked_element = $(href);
-      if (linked_element.length === 1) {
+      const linkedElement = $(href);
+      if (linkedElement.length === 1) {
         a.closest('li').show();
-        if (active_a === null || window.scrollY + scroll_offset >= linked_element.offset().top) {
-          active_a = a;
+        if (activeA === null || window.scrollY + scrollOffset >= linkedElement.offset().top) {
+          activeA = a;
         }
       } else {
         a.closest('li').hide();
       }
     });
-    if (active_a !== null) {
-      active_a.addClass('active');
+    if (activeA !== null) {
+      activeA.addClass('active');
       // show back to top link if sidebar is used
       $('#sidebar > nav a[href="#top"]').show();
     }
   }).trigger('scroll');
 
-  $(".navbar-nav > .dropdown").hover(function(){
-    var dropdown_menu = $(this).children(".dropdown-menu");
-    if(dropdown_menu.is(":visible")){
-      dropdown_menu.parent().toggleClass("open");
+  // show navbar dropdowns on hover
+  $('.navbar-nav > .dropdown').hover(function () {
+    const dropdownMenu = $(this).children('.dropdown-menu');
+    if (dropdownMenu.is(':visible')) {
+      dropdownMenu.parent().toggleClass('open');
     }
   });
+
+  // update text for search field when advanced search is toggled
   $('#input-search-advanced').on('change', function () {
     if (this.checked) {
       $('#input-search').attr('placeholder', window.template_values.translations.advanced_search);
@@ -62,6 +73,8 @@ $(function() {
       $('#input-search').attr('placeholder', window.template_values.translations.advanced_search);
     }
   }).change();
+
+  // fullscreen image preview event handlers
   $('.fullscreen-image-preview').on('click', function (e) {
     $(this).hide();
   });
@@ -75,36 +88,37 @@ $(function() {
     $(this).next('.fullscreen-image-preview').css('display', 'flex');
   });
 
-if (window.template_values.notifications.num_urgent_notifications) {
-  let urgent_notification_modal = $('#urgentNotificationModal');
-  urgent_notification_modal.on('shown.bs.modal', function () {
-    urgent_notification_modal.focus();
-  });
-  $('.urgent-notifications-counter').on('click', function () {
-    urgent_notification_modal.modal();
-  });
-  urgent_notification_modal.find('#button-hide-urgent-notifications').on('click', function () {
-    Cookies.set('sampledb-hide-urgent-notifications-until', moment().add(1, 'hour').format(), {
-      expires: 1,
-      SameSite: 'Lax'
+  // show urgent notifications
+  if (window.template_values.notifications.num_urgent_notifications) {
+    const urgentNotificationModal = $('#urgentNotificationModal');
+    urgentNotificationModal.on('shown.bs.modal', function () {
+      urgentNotificationModal.focus();
     });
-    setTimeout(function () {
-      urgent_notification_modal.modal();
-    }, 60 * 60 * 1000);
-  });
-  let hide_notifications = window.template_values.notifications.hide_notifications;
-  let hiding_time = Cookies.get('sampledb-hide-urgent-notifications-until');
-  if (hiding_time) {
-    hiding_time = moment(hiding_time);
-    if (hiding_time && moment().isBefore(hiding_time)) {
-      hide_notifications = true;
+    $('.urgent-notifications-counter').on('click', function () {
+      urgentNotificationModal.modal();
+    });
+    urgentNotificationModal.find('#button-hide-urgent-notifications').on('click', function () {
+      Cookies.set('sampledb-hide-urgent-notifications-until', moment().add(1, 'hour').format(), {
+        expires: 1,
+        SameSite: 'Lax'
+      });
       setTimeout(function () {
-        urgent_notification_modal.modal();
-      }, hiding_time.diff(moment(), 'ms'));
+        urgentNotificationModal.modal();
+      }, 60 * 60 * 1000);
+    });
+    let hideNotifications = window.template_values.notifications.hide_notifications;
+    let hidingTime = Cookies.get('sampledb-hide-urgent-notifications-until');
+    if (hidingTime) {
+      hidingTime = moment(hidingTime);
+      if (hidingTime && moment().isBefore(hidingTime)) {
+        hideNotifications = true;
+        setTimeout(function () {
+          urgentNotificationModal.modal();
+        }, hidingTime.diff(moment(), 'ms'));
+      }
+    }
+    if (!hideNotifications) {
+      urgentNotificationModal.modal();
     }
   }
-  if (!hide_notifications) {
-    urgent_notification_modal.modal();
-  }
-}
 });
