b22349a5b94583c051b9a4b24a054a7ee8e9aa15
Florian Rhiem <f.rhiem@fz-juelich.de>
2024-01-02 13:46:00 +0100

    Move javascript from group.html to module

---
+++ sampledb/static/sampledb/js/page/group.js
@@ -0,0 +1,39 @@
+
+import {
+  updateTranslationJSON,
+  setTranslationHandler,
+  updateTranslationLanguages
+} from '../sampledb-internationalization.js';
+
+$(function (){
+  window.translations = window.getTemplateValue('translations');
+
+  window.languages = window.getTemplateValue('language_info.languages');
+  updateTranslationJSON();
+  $('#select-language-name').selectpicker('val', window.getTemplateValue('name_language_ids').map(String));
+  $('#select-language-description').selectpicker('val', window.getTemplateValue('description_language_ids').map(String));
+
+  $('#select-language-name').on('change', function () {
+    updateTranslationLanguages(this, 'name-template', 'input-name-', ['name', 'description']);
+  }).change();
+  $('#select-language-description').on('change', function () {
+    updateTranslationLanguages(this, 'description-template', 'input-description-', ['name', 'description']);
+  }).change();
+
+  $('[data-name="input-names"] [data-language-id], [data-name="input-descriptions"] [data-language-id]').each(function() {
+    setTranslationHandler(this);
+  });
+
+  $('form').on('submit', function() {
+    $('input').change();
+    $('textarea').change();
+    updateTranslationJSON();
+    return $(this).find('.has-error').length === 0;
+  })
+  if (window.getTemplateValue('show_edit_form')) {
+    var edit_modal = $('#editGroupModal');
+    edit_modal.removeClass('fade');
+    edit_modal.modal('show');
+    edit_modal.addClass('fade');
+  }
+});
