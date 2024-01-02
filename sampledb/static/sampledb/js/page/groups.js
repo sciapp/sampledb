ea67ed2f694fd2f3e4310a4b540601e1eb4bafd5
Florian Rhiem <f.rhiem@fz-juelich.de>
2024-01-02 14:01:36 +0100

    Fix static checks for groups.js

--- sampledb/static/sampledb/js/page/groups.js
+++ sampledb/static/sampledb/js/page/groups.js
@@ -1,16 +1,19 @@
+'use strict';
+/* eslint-env jquery */
+
 import {
   updateTranslationJSON,
   setTranslationHandler,
   updateTranslationLanguages
 } from '../sampledb-internationalization.js';
 
-$(function (){
+$(function () {
   window.translations = [];
 
   window.languages = window.getTemplateValue('language_info.languages');
   updateTranslationJSON();
 
-  $('.select-language').selectpicker('val', ['' + window.getTemplateValue('language_info.english_id')])
+  $('.select-language').selectpicker('val', ['' + window.getTemplateValue('language_info.english_id')]);
 
   $('#select-language-name').on('change', function () {
     updateTranslationLanguages(this, 'name-template', 'input-name-', ['name', 'description']);
@@ -24,22 +27,22 @@ $(function (){
     setTranslationHandler(this);
   });
 
-  $('form').on('submit', function() {
+  $('form').on('submit', function () {
     $('input').change();
     $('textarea').change();
-    window.translations = $.map(window.translations, function(translation, index){
-      if (translation.name  === '' && translation.description === '' && translation.language_id !== '' + window.getTemplateValue('language_info.english_id')){
+    window.translations = $.map(window.translations, function (translation, index) {
+      if (translation.name === '' && translation.description === '' && translation.language_id !== '' + window.getTemplateValue('language_info.english_id')) {
         return null;
       }
-      return translation
+      return translation;
     });
     updateTranslationJSON();
     return $(this).find('.has-error').length === 0;
   });
   if (window.getTemplateValue('show_create_form')) {
-    var create_modal = $('#createGroupModal');
-    create_modal.removeClass('fade');
-    create_modal.modal('show');
-    create_modal.addClass('fade');
+    const createModal = $('#createGroupModal');
+    createModal.removeClass('fade');
+    createModal.modal('show');
+    createModal.addClass('fade');
   }
 });
