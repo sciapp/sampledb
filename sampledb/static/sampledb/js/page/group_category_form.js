9c6c1d30af7c0a9d66eddac4634de4171225b06f
Florian Rhiem <f.rhiem@fz-juelich.de>
2024-01-02 14:36:19 +0100

    Fix static checks for group_category_form.js

--- sampledb/static/sampledb/js/page/group_category_form.js
+++ sampledb/static/sampledb/js/page/group_category_form.js
@@ -1,13 +1,16 @@
-$(function(){
-  const language_picker = $('#select-languages');
-  function updateEnabledLanguages() {
-    let language_ids = $.map(language_picker.selectpicker('val'), function(language_id) {
-      return +language_id;
+'use strict';
+/* eslint-env jquery */
+
+$(function () {
+  const languagePicker = $('#select-languages');
+  function updateEnabledLanguages () {
+    const languageIDs = $.map(languagePicker.selectpicker('val'), function (languageID) {
+      return +languageID;
     });
-    language_ids.push(window.getTemplateValue('language_info.english_id'));
-    $('[data-language-id]').each(function(_, e) {
-      let element = $(e);
-      if (language_ids.includes(+element.data('languageId'))) {
+    languageIDs.push(window.getTemplateValue('language_info.english_id'));
+    $('[data-language-id]').each(function (_, e) {
+      const element = $(e);
+      if (languageIDs.includes(+element.data('languageId'))) {
         element.show();
         element.find('input').prop('disabled', false);
       } else {
@@ -16,23 +19,23 @@ $(function(){
       }
     });
   }
-  language_picker.on('changed.bs.select', updateEnabledLanguages);
+  languagePicker.on('changed.bs.select', updateEnabledLanguages);
   updateEnabledLanguages();
 
   // enforce text length limits
-  $('[data-language-id] input').on('change', function(e) {
+  $('[data-language-id] input').on('change', function (e) {
     const input = $(this);
-    const input_text = input.val();
-    let error_text = '';
-    if (input_text.length === 0) {
-      error_text = input.data('emptyText');
-    } else if (input_text.length > input.data('maxLength')) {
-      error_text = input.data('maxLengthText');
+    const inputText = input.val();
+    let errorText = '';
+    if (inputText.length === 0) {
+      errorText = input.data('emptyText');
+    } else if (inputText.length > input.data('maxLength')) {
+      errorText = input.data('maxLengthText');
     }
-    const input_group = input.parent();
-    const input_help_block = input_group.next('.help-block');
-    input_help_block.text(error_text);
+    const inputGroup = input.parent();
+    const inputHelpBlock = inputGroup.next('.help-block');
+    inputHelpBlock.text(errorText);
 
-    input_group.parent().toggleClass('has-error', error_text !== '');
+    inputGroup.parent().toggleClass('has-error', errorText !== '');
   }).change();
 });
