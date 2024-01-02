c87ed6ba89f17c1422196dc405396ed90dc1c396
Florian Rhiem <f.rhiem@fz-juelich.de>
2024-01-02 15:12:55 +0100

    Fix static checks for location_form.js

--- sampledb/static/sampledb/js/page/location_form.js
+++ sampledb/static/sampledb/js/page/location_form.js
@@ -1,17 +1,20 @@
+'use strict';
+/* eslint-env jquery */
+
 import {
   updateTranslationJSON,
   setTranslationHandler,
   updateTranslationLanguages
 } from '../sampledb-internationalization.js';
 
-$(function (){
+$(function () {
   window.translations = window.getTemplateValue('translations');
   if (window.translations.length === 0) {
     window.translations.push({
-      'language_id': '' + window.getTemplateValue('language_info.english_id'),
-      'lang_name': '',
-      'name': '',
-      'description': ''
+      language_id: '' + window.getTemplateValue('language_info.english_id'),
+      lang_name: '',
+      name: '',
+      description: ''
     });
   }
 
@@ -28,53 +31,53 @@ $(function (){
     updateTranslationLanguages(this, 'description-template', 'input-description-', ['name', 'description']);
   }).change();
 
-  $('[data-name="input-names"] [data-language-id], [data-name="input-descriptions"] [data-language-id]').each(function() {
+  $('[data-name="input-names"] [data-language-id], [data-name="input-descriptions"] [data-language-id]').each(function () {
     setTranslationHandler(this);
   });
 
-  $('form').on('submit', function() {
+  $('form').on('submit', function () {
     $('input').change();
     $('textarea').change();
-    window.translations = $.map(window.translations, function(translation, index){
-      if (translation.name  === '' && translation.description === '' && translation.language_id !== window.getTemplateValue('language_info.english_id')){
+    window.translations = $.map(window.translations, function (translation, index) {
+      if (translation.name === '' && translation.description === '' && translation.language_id !== window.getTemplateValue('language_info.english_id')) {
         return null;
       }
-      return translation
+      return translation;
     });
     updateTranslationJSON();
     return $(this).find('.has-error').length === $(this).find('.has-error.may-have-error').length;
   });
 
-  let location_type_picker = $('select#input-location-type');
-  location_type_picker.on('changed.bs.select', function() {
-      let selected_option = location_type_picker.find('option:selected');
-      if (selected_option.data('enableParentLocation')) {
-          $('select#input-parent-location').prop('disabled', false).closest('.form-group').show();
-          $('input#input-parent-location-replacement').prop('disabled', true);
-      } else {
-          $('select#input-parent-location').prop('disabled', true).closest('.form-group').hide();
-          $('input#input-parent-location-replacement').prop('disabled', false);
-      }
-      if (selected_option.data('enableResponsibleUsers')) {
-          $('select#input-responsible_users').prop('disabled', false).closest('.form-group').show();
-      } else {
-          $('select#input-responsible_users').prop('disabled', true).closest('.form-group').hide();
-      }
-      if (selected_option.data('enableCapacities')) {
-          $('#capacity-field-list').show();
-      } else {
-          $('#capacity-field-list').hide();
-      }
-  }).trigger('changed.bs.select');
-  $('.capacity-toggle').on('change', function() {
-    let capacity_toggle = $(this)
-    let capacity_input = capacity_toggle.closest('.form-group').find('input[type="number"]');
-    if (capacity_toggle.prop('checked')) {
-      capacity_input.prop('disabled', true);
+  const locationTypePicker = $('select#input-location-type');
+  locationTypePicker.on('changed.bs.select', function () {
+    const selectedOption = locationTypePicker.find('option:selected');
+    if (selectedOption.data('enableParentLocation')) {
+      $('select#input-parent-location').prop('disabled', false).closest('.form-group').show();
+      $('input#input-parent-location-replacement').prop('disabled', true);
     } else {
-      capacity_input.prop('disabled', false);
-      if (capacity_input.val() === "") {
-        capacity_input.val("0");
+      $('select#input-parent-location').prop('disabled', true).closest('.form-group').hide();
+      $('input#input-parent-location-replacement').prop('disabled', false);
+    }
+    if (selectedOption.data('enableResponsibleUsers')) {
+      $('select#input-responsible_users').prop('disabled', false).closest('.form-group').show();
+    } else {
+      $('select#input-responsible_users').prop('disabled', true).closest('.form-group').hide();
+    }
+    if (selectedOption.data('enableCapacities')) {
+      $('#capacity-field-list').show();
+    } else {
+      $('#capacity-field-list').hide();
+    }
+  }).trigger('changed.bs.select');
+  $('.capacity-toggle').on('change', function () {
+    const capacityToggle = $(this);
+    const capacityInput = capacityToggle.closest('.form-group').find('input[type="number"]');
+    if (capacityToggle.prop('checked')) {
+      capacityInput.prop('disabled', true);
+    } else {
+      capacityInput.prop('disabled', false);
+      if (capacityInput.val() === '') {
+        capacityInput.val('0');
       }
     }
   }).change();
