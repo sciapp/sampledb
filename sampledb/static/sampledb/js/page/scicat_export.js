6b08e7c80257b3708e311af95c6e6d1beb42accc
Florian Rhiem <f.rhiem@fz-juelich.de>
2024-01-03 12:58:27 +0100

    Fix static checks for scicat_export.js

--- sampledb/static/sampledb/js/page/scicat_export.js
+++ sampledb/static/sampledb/js/page/scicat_export.js
@@ -1,8 +1,11 @@
-$(function() {
-  $('.scicat-export-select-all').click(function() {
+'use strict';
+/* eslint-env jquery */
+
+$(function () {
+  $('.scicat-export-select-all').click(function () {
     $(this).parent().parent().find('input[type="checkbox"]:not(:disabled)').prop('checked', true);
   });
-  $('.scicat-export-select-none').click(function() {
+  $('.scicat-export-select-none').click(function () {
     $(this).parent().parent().find('input[type="checkbox"]:not(:disabled)').prop('checked', false);
   });
 });
