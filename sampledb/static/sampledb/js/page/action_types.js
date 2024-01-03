31c5111e1a2445de5b18d2805f6311ed4f4bf967
Florian Rhiem <f.rhiem@fz-juelich.de>
2024-01-03 09:42:02 +0100

    Fix static checks for action_types.js

--- sampledb/static/sampledb/js/page/action_types.js
+++ sampledb/static/sampledb/js/page/action_types.js
@@ -1,12 +1,16 @@
-$(document).ready(function() {
+'use strict';
+/* eslint-env jquery */
+/* globals Sortable */
+
+$(function () {
   if ($('#sortingModal').length > 0) {
-    let elem = document.getElementById('actionTypesModalList');
-    let options = {
+    const elem = document.getElementById('actionTypesModalList');
+    const options = {
       handle: '.handle',
       animation: 150,
       dataIdAttr: 'data-id'
-    }
-    let sortable = new Sortable(elem, options);
+    };
+    const sortable = new Sortable(elem, options);
     $('#form-sort-order').on('submit', function () {
       document.getElementById('encoded_order').value = sortable.toArray().join();
     });
