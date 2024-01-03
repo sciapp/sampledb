a2d1383ca66056c8a48bdd39f58d783130e3f5eb
Florian Rhiem <f.rhiem@fz-juelich.de>
2024-01-03 12:56:47 +0100

    Move javascript from scicat_export.html to module

---
+++ sampledb/static/sampledb/js/page/scicat_export.js
@@ -0,0 +1,8 @@
+$(function() {
+  $('.scicat-export-select-all').click(function() {
+    $(this).parent().parent().find('input[type="checkbox"]:not(:disabled)').prop('checked', true);
+  });
+  $('.scicat-export-select-none').click(function() {
+    $(this).parent().parent().find('input[type="checkbox"]:not(:disabled)').prop('checked', false);
+  });
+});
