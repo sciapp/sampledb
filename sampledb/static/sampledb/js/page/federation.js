28b234e6a9ebb97da334a34ee3483bec99a9c594
Florian Rhiem <f.rhiem@fz-juelich.de>
2024-01-03 10:40:12 +0100

    Fix static checks for federation.js

--- sampledb/static/sampledb/js/page/federation.js
+++ sampledb/static/sampledb/js/page/federation.js
@@ -1,17 +1,21 @@
+'use strict';
+/* eslint-env jquery */
+/* globals vis */
+
 if (window.getTemplateValue('show_add_form')) {
   $(document).ready(function () {
-    var add_modal = $('#addComponentModal');
-    add_modal.removeClass('fade');
-    add_modal.modal('show');
-    add_modal.addClass('fade');
+    const addModal = $('#addComponentModal');
+    addModal.removeClass('fade');
+    addModal.modal('show');
+    addModal.addClass('fade');
   });
 }
 if (window.getTemplateValue('federation_uuid') === null) {
   $('span.copy-uuid').on('click', function () {
-    var button = $(this);
+    const button = $(this);
     navigator.clipboard.writeText(button.attr('data-uuid')).then(
       () => {
-        var wrapper = button.parent();
+        const wrapper = button.parent();
         button.tooltip('hide');
         wrapper.tooltip('show');
         setTimeout(function () {
@@ -21,60 +25,60 @@ if (window.getTemplateValue('federation_uuid') === null) {
     );
   });
 }
-$(function() {
-  let address_input = $('#addComponentAddress');
-  let uuid_input = $('#addComponentUUID');
-  let name_input = $('#addComponentName');
-  let address_group = address_input.closest('.form-group');
-  let uuid_group = uuid_input.closest('.form-group');
-  let name_group = name_input.closest('.form-group');
-  address_input.on('change', function() {
-    address_group.removeClass('has-error').removeClass('has-success');
-    address_group.find('.help-block').remove();
-    let database_url = address_input.val();
-    if (database_url) {
-      if (!database_url.endsWith('/')) {
-        database_url = database_url + '/'
+$(function () {
+  const addressInput = $('#addComponentAddress');
+  const uuidInput = $('#addComponentUUID');
+  const nameInput = $('#addComponentName');
+  const addressGroup = addressInput.closest('.form-group');
+  const uuidGroup = uuidInput.closest('.form-group');
+  const nameGroup = nameInput.closest('.form-group');
+  addressInput.on('change', function () {
+    addressGroup.removeClass('has-error').removeClass('has-success');
+    addressGroup.find('.help-block').remove();
+    let databaseURL = addressInput.val();
+    if (databaseURL) {
+      if (!databaseURL.endsWith('/')) {
+        databaseURL = databaseURL + '/';
       }
-      let status_url = database_url + 'status/';
-      $.get(status_url, function(data) {
-        address_group.addClass('has-success');
-        if (!uuid_input.val() && data.federation_uuid && !name_input.val() && data.name) {
-          uuid_input.val(data.federation_uuid);
-          name_input.val(data.name);
-          uuid_group.removeClass('has-error').addClass('has-success');
-          name_group.removeClass('has-error').addClass('has-success');
-          uuid_group.find('.help-block').remove();
-          name_group.find('.help-block').remove();
+      const statusURL = databaseURL + 'status/';
+      $.get(statusURL, function (data) {
+        addressGroup.addClass('has-success');
+        if (!uuidInput.val() && data.federation_uuid && !nameInput.val() && data.name) {
+          uuidInput.val(data.federation_uuid);
+          nameInput.val(data.name);
+          uuidGroup.removeClass('has-error').addClass('has-success');
+          nameGroup.removeClass('has-error').addClass('has-success');
+          uuidGroup.find('.help-block').remove();
+          nameGroup.find('.help-block').remove();
         }
       });
     }
   });
-  uuid_input.on('change', function() {
-    uuid_group.removeClass('has-error').removeClass('has-success');
-    uuid_group.find('.help-block').remove();
+  uuidInput.on('change', function () {
+    uuidGroup.removeClass('has-error').removeClass('has-success');
+    uuidGroup.find('.help-block').remove();
   });
-  name_input.on('change', function() {
-    name_group.removeClass('has-error').removeClass('has-success');
-    name_group.find('.help-block').remove();
+  nameInput.on('change', function () {
+    nameGroup.removeClass('has-error').removeClass('has-success');
+    nameGroup.find('.help-block').remove();
   });
   if (window.getTemplateValue('nodes').length > 0 && window.getTemplateValue('edges').length > 0) {
     // create an array with nodes
-    let nodes = new vis.DataSet(
+    const nodes = new vis.DataSet(
       window.getTemplateValue('nodes')
     );
     // create an array with edges
-    let edges = new vis.DataSet(
+    const edges = new vis.DataSet(
       window.getTemplateValue('edges')
     );
 
     // create a network
-    let container = document.getElementById("graph");
-    let data = {
-      nodes: nodes,
-      edges: edges
+    const container = document.getElementById('graph');
+    const data = {
+      nodes,
+      edges
     };
-    let options = {
+    const options = {
       layout: {
         randomSeed: 9 // prevent the graph from being random
       },
@@ -104,6 +108,6 @@ $(function() {
         }
       }
     };
-    let network = new vis.Network(container, data, options);
+    window.network = new vis.Network(container, data, options);
   }
 });
