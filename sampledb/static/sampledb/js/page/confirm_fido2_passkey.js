ab657d4403bdfce17e8b232351a1162def887109
Florian Rhiem <f.rhiem@fz-juelich.de>
2024-01-30 13:08:37 +0100

    Fix static checks for confirm_fido2_passkey.html

--- sampledb/static/sampledb/js/page/confirm_fido2_passkey.js
+++ sampledb/static/sampledb/js/page/confirm_fido2_passkey.js
@@ -1,18 +1,23 @@
+'use strict';
+/* eslint-env jquery */
+/* jshint esversion: 8 */
 
 import {
   get,
   supported,
-  parseRequestOptionsFromJSON,
+  parseRequestOptionsFromJSON
 } from '../../../webauthn-json/js/webauthn-json.browser-ponyfill.js';
 
-async function authenticate(e) {
-  e && e.preventDefault();
+async function authenticate (e) {
+  if (e) {
+    e.preventDefault();
+  }
   const options = window.getTemplateValue('fido2_options');
   const publicKeyCredentialRequestOptions = parseRequestOptionsFromJSON(options);
   const form = $('#fido2-authenticate-form');
   form.removeClass('has-error');
-  const help_bock = form.find('.help-block');
-  help_bock.text('');
+  const helpBock = form.find('.help-block');
+  helpBock.text('');
   try {
     const assertion = await get(publicKeyCredentialRequestOptions);
     form.find('[name="assertion"]').val(JSON.stringify(assertion));
@@ -20,13 +25,14 @@ async function authenticate(e) {
     return true;
   } catch (error) {
     form.addClass('has-error');
-    help_bock.text(window.getTemplateValue('translations.passkey_failed') + error)
+    helpBock.text(window.getTemplateValue('translations.passkey_failed') + error);
     return false;
   }
 }
-$(function() {
+
+$(function () {
   if (supported()) {
-    $("#fido2-authenticate-form").on('submit', authenticate);
+    $('#fido2-authenticate-form').on('submit', authenticate);
   } else {
     $('#alert-fido2-not-supported').show();
     $('#fido2-authenticate-form input').prop('disabled', true);
