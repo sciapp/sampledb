5d848b0aedfbdfd4bd54c420dce948a01cbe56c8
Florian Rhiem <f.rhiem@fz-juelich.de>
2024-01-30 13:52:56 +0100

    Fix static checks for preferences.js

--- sampledb/static/sampledb/js/page/preferences.js
+++ sampledb/static/sampledb/js/page/preferences.js
@@ -1,79 +1,84 @@
- import {
+'use strict';
+/* eslint-env jquery */
+/* jshint esversion: 8 */
+/* globals moment */
+
+import {
   create,
   supported,
   parseCreationOptionsFromJSON
 } from '../../../webauthn-json/js/webauthn-json.browser-ponyfill.js';
 
-if (window.getTemplateValue("config.timezone_not_set")) {
-  const tz_pref = moment.tz.guess(true);
-  var auto_tz = document.getElementById("auto_tz");
-  auto_tz.textContent += tz_pref;
+if (window.getTemplateValue('config.timezone_not_set')) {
+  const localTimezone = moment.tz.guess(true);
+  const automaticTimezoneField = document.getElementById('auto_tz');
+  automaticTimezoneField.textContent += localTimezone;
 }
 
-if (window.getTemplateValue("has_created_api_token")) {
+if (window.getTemplateValue('has_created_api_token')) {
   $('#viewApiTokenModal').modal('show');
-  $('#api-token button').bind('click', function() {
-    var input = document.querySelector('#api-token input');
+  $('#api-token button').bind('click', function () {
+    const input = document.querySelector('#api-token input');
     input.setSelectionRange(0, input.value.length + 1);
     $(input).focus();
     try {
-      var success = document.execCommand('copy');
+      const success = document.execCommand('copy');
       if (success) {
-        $('#api-token-copy-notes').text(window.getTemplateValue("translations.copied"));
+        $('#api-token-copy-notes').text(window.getTemplateValue('translations.copied'));
         $('#api-token').removeClass('has-error').addClass('has-success');
       } else {
-        $('#api-token-copy-notes').text(window.getTemplateValue("translations.could_not_copy"));
+        $('#api-token-copy-notes').text(window.getTemplateValue('translations.could_not_copy'));
         $('#api-token').removeClass('has-success').addClass('has-error');
       }
     } catch (err) {
-        $('#api-token-copy-notes').text(window.getTemplateValue("translations.could_not_copy"));
-        $('#api-token').removeClass('has-success').addClass('has-error');
+      $('#api-token-copy-notes').text(window.getTemplateValue('translations.could_not_copy'));
+      $('#api-token').removeClass('has-success').addClass('has-error');
     }
   });
 }
 
-if (window.getTemplateValue("show_api_token_modal")) {
+if (window.getTemplateValue('show_api_token_modal')) {
   $('#createApiTokenModal').modal('show');
 }
 
-if (window.getTemplateValue("show_add_webhook_form")) {
-  $(document).ready(function() {
-    var add_modal = $('#addWebhookModal');
-    add_modal.removeClass('fade');
-    add_modal.modal('show');
-    add_modal.addClass('fade');
+if (window.getTemplateValue('show_add_webhook_form')) {
+  $(document).ready(function () {
+    const addWebhookModal = $('#addWebhookModal');
+    addWebhookModal.removeClass('fade');
+    addWebhookModal.modal('show');
+    addWebhookModal.addClass('fade');
   });
 }
 
-if (window.getTemplateValue("has_created_webhook_secret")) {
+if (window.getTemplateValue('has_created_webhook_secret')) {
   $('#viewWebhookSecretModal').modal('show');
-  $('#webhook-secret button').bind('click', function() {
-    var input = document.querySelector('#webhook-secret input');
+  $('#webhook-secret button').bind('click', function () {
+    const input = document.querySelector('#webhook-secret input');
     input.setSelectionRange(0, input.value.length + 1);
     $(input).focus();
     try {
-      var success = document.execCommand('copy');
+      const success = document.execCommand('copy');
       if (success) {
-        $('#webhook-secret-copy-notes').text(window.getTemplateValue("translations.copied"));
+        $('#webhook-secret-copy-notes').text(window.getTemplateValue('translations.copied'));
         $('#webhook-secret').removeClass('has-error').addClass('has-success');
       } else {
-        $('#webhook-secret-copy-notes').text(window.getTemplateValue("translations.could_not_copy"));
+        $('#webhook-secret-copy-notes').text(window.getTemplateValue('translations.could_not_copy'));
         $('#webhook-secret').removeClass('has-success').addClass('has-error');
       }
     } catch (err) {
-        $('#webhook-secret-copy-notes').text(window.getTemplateValue("translations.could_not_copy"));
-        $('#webhook-secret').removeClass('has-success').addClass('has-error');
+      $('#webhook-secret-copy-notes').text(window.getTemplateValue('translations.could_not_copy'));
+      $('#webhook-secret').removeClass('has-success').addClass('has-error');
     }
   });
 }
 
-export async function register() {
+export async function register () {
   const options = window.getTemplateValue('fido2_options');
   const form = $('#form-add-authentication-methods');
-  const type_form_group = $('#addAuthenticationMethodTypeSelect').closest('.form-group');
-  type_form_group.removeClass('has-error');
-  const help_block = type_form_group.find('.help-block');
-  help_block.text('');
+  const typeFormGroup = $('#addAuthenticationMethodTypeSelect').closest('.form-group');
+  typeFormGroup.removeClass('has-error');
+  const helpBlock = typeFormGroup.find('.help-block');
+  helpBlock.text('');
   try {
     const publicKeyCredentialCreationOptions = parseCreationOptionsFromJSON(options);
     const credential = await create(publicKeyCredentialCreationOptions);
@@ -81,29 +86,31 @@ export async function register() {
     form[0].submit();
     return true;
   } catch (error) {
-    type_form_group.addClass('has-error');
-    help_block.text(window.getTemplateValue('translations.passkey_failed') + error);
+    typeFormGroup.addClass('has-error');
+    helpBlock.text(window.getTemplateValue('translations.passkey_failed') + error);
     return false;
   }
 }
 
-$(function() {
-  const add_authentication_method_type_select = $('#addAuthenticationMethodTypeSelect');
-  add_authentication_method_type_select.on('changed.bs.select', function () {
-    if (add_authentication_method_type_select.val() === 'FIDO2_PASSKEY') {
+$(function () {
+  const addAuthenticationMethodTypeSelect = $('#addAuthenticationMethodTypeSelect');
+  addAuthenticationMethodTypeSelect.on('changed.bs.select', function () {
+    if (addAuthenticationMethodTypeSelect.val() === 'FIDO2_PASSKEY') {
       $('#addAuthenticationMethodDescriptionGroup').show().find('input').prop('disabled', false);
       $('#addAuthenticationMethodLoginGroup, #addAuthenticationMethodPasswordGroup').hide().find('input').prop('disabled', true);
     } else {
-      add_authentication_method_type_select.closest('.form-group').removeClass('has-error').find('.help-block').text('');
+      addAuthenticationMethodTypeSelect.closest('.form-group').removeClass('has-error').find('.help-block').text('');
       $('#addAuthenticationMethodDescriptionGroup').hide().find('input').prop('disabled', true);
       $('#addAuthenticationMethodLoginGroup, #addAuthenticationMethodPasswordGroup').show().find('input').prop('disabled', false);
     }
   });
   if (supported()) {
     $('#form-add-authentication-methods').on('submit', function (event) {
-      if (add_authentication_method_type_select.val() === 'FIDO2_PASSKEY') {
-        event && event.preventDefault();
-        if ($("#input-passkey-description").val() === "") {
+      if (addAuthenticationMethodTypeSelect.val() === 'FIDO2_PASSKEY') {
+        if (event) {
+          event.preventDefault();
+        }
+        if ($('#input-passkey-description').val() === '') {
           $('#addAuthenticationMethodDescriptionGroup').addClass('has-error').find('.help-block:last').text(window.getTemplateValue('translations.enter_at_least_1_character'));
           return false;
         } else {
@@ -114,11 +121,11 @@ $(function() {
       return true;
     });
   } else {
-    if (add_authentication_method_type_select.val() === 'FIDO2_PASSKEY') {
-      add_authentication_method_type_select.val('E');
+    if (addAuthenticationMethodTypeSelect.val() === 'FIDO2_PASSKEY') {
+      addAuthenticationMethodTypeSelect.val('E');
     }
-    add_authentication_method_type_select.find('option[value="FIDO2_PASSKEY"]').prop('disabled', true);
-    add_authentication_method_type_select.selectpicker('refresh');
+    addAuthenticationMethodTypeSelect.find('option[value="FIDO2_PASSKEY"]').prop('disabled', true);
+    addAuthenticationMethodTypeSelect.selectpicker('refresh');
   }
-  add_authentication_method_type_select.trigger('changed.bs.select');
+  addAuthenticationMethodTypeSelect.trigger('changed.bs.select');
 });
