a3b8266aa79e3e998dce9c14a91a17366de1630d
Florian Rhiem <f.rhiem@fz-juelich.de>
2024-02-13 08:23:59 +0100

    Prepare release v0.26.0

--- sampledb/version.py
+++ sampledb/version.py
@@ -11,4 +11,4 @@ pipeline to provide the current version as returned by git describe.
 
 import os
 
-__version__ = os.environ.get('SAMPLEDB_VERSION') or '0.25.3'
+__version__ = os.environ.get('SAMPLEDB_VERSION') or '0.26.0'
