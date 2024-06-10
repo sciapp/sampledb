75a2c83e26d09d46dcae813d4c73bbc858b35dc4
Florian Rhiem <f.rhiem@fz-juelich.de>
2024-06-10 10:34:53 +0200

    Use explicit list of migrations instead of linking from migration to previous migration to automatically detect conflicts

--- sampledb/models/migrations/__init__.py
+++ sampledb/models/migrations/__init__.py
@@ -6,11 +6,11 @@ import sys
 
 import flask_sqlalchemy
 
-from .utils import find_migrations, should_skip_by_index, update_migration_index
+from .utils import get_migrations, should_skip_by_index, update_migration_index
 
 
 def run(db: flask_sqlalchemy.SQLAlchemy) -> None:
-    for index, name, function in find_migrations():
+    for index, name, function in get_migrations():
 
         # Skip migration by migration index
         if should_skip_by_index(db, index):
--- sampledb/models/migrations/utils.py
+++ sampledb/models/migrations/utils.py
@@ -4,7 +4,6 @@
 """
 
 import importlib
-import os
 import sys
 import typing
 
@@ -58,46 +57,203 @@ def update_migration_index(db: typing.Any, index: int) -> None:
     ).rowcount == 1
 
 
-def find_migrations() -> typing.List[typing.Tuple[int, str, typing.Callable[[typing.Any], bool]]]:
+def get_migrations() -> typing.List[typing.Tuple[int, str, typing.Callable[[typing.Any], bool]]]:
     """
     Finds migrations and returns them sorted by their index.
 
     :return: A list of migrations, with each migration as a tuple of its
         index, name and function.
     """
-    migrations_dir = os.path.abspath(os.path.dirname(__file__))
+    migration_module_names = [
+        "create_migration_index",
+        "actions_add_user_id",
+        "object_log_entry_type_add_assign_location",
+        "user_log_entry_type_add_assign_location",
+        "user_log_entry_type_add_create_location",
+        "user_log_entry_type_add_update_location",
+        "object_location_assignments_add_responsible_user_id",
+        "create_user_object_permissions_by_all_view",
+        "files_add_data",
+        "user_log_entry_type_add_link_publication",
+        "object_log_entry_type_add_link_publication",
+        "object_location_assignments_add_confirmed",
+        "user_add_is_readonly",
+        "user_add_is_hidden",
+        "authentication_type_add_api_token",
+        "actions_add_description_as_html",
+        "instruments_add_description_as_html",
+        "user_add_orcid",
+        "user_log_entry_type_add_create_instrument_log_entry",
+        "notification_type_add_instrument_log_entry_created",
+        "instruments_add_users_can_create_log_entries",
+        "instruments_add_users_can_view_log_entries",
+        "instruments_add_notes",
+        "instruments_add_notes_as_html",
+        "user_add_affiliation",
+        "instruments_add_create_log_entry_default",
+        "actions_add_is_hidden",
+        "publications_add_object_name",
+        "instruments_add_is_hidden",
+        "notification_type_add_referenced_by_object_metadata",
+        "action_type_create_default_action_types",
+        "action_replace_type_with_type_id",
+        "object_log_entry_type_reference_object_in_metadata",
+        "user_add_is_active",
+        "notification_type_add_instrument_log_entry_edited",
+        "instrument_log_entries_add_versioning",
+        "object_log_entry_type_export_to_dataverse",
+        "actions_replace_description_as_html_with_description_is_markdown",
+        "instruments_replace_description_as_html_with_description_is_markdown",
+        "instruments_replace_notes_as_html_with_notes_is_markdown",
+        "instruments_add_short_description",
+        "actions_add_short_description",
+        "markdown_to_html_cache_recreate",
+        "action_type_add_enable_project_link",
+        "object_log_entry_type_add_link_project",
+        "object_log_entry_type_add_unlink_project",
+        "instrument_log_entry_versions_add_content_is_markdown",
+        "public_actions_add_common_actions",
+        "instrument_log_entry_versions_add_event_utc_datetime",
+        "languages_create_default_languages",
+        "actions_move_to_action_translations",
+        "action_type_move_to_translation",
+        "action_type_translations_create_default_translations",
+        "instruments_move_to_translations",
+        "locations_replace_string_columns_with_JSON",
+        "groups_replace_string_columns_with_JSON",
+        "projects_replace_string_columns_with_JSON",
+        "location_assignments_replace_string_columns_with_JSON",
+        "user_add_role",
+        "languages_add_enabled_for_user_interface",
+        "action_type_drop_description",
+        "actions_drop_description_as_html",
+        "actions_drop_short_description_as_html",
+        "instruments_drop_description_as_html",
+        "instruments_drop_short_description_as_html",
+        "instruments_drop_notes_as_html",
+        "user_add_extra_fields",
+        "files_add_binary_data",
+        "action_type_add_disable_create_objects",
+        "action_type_add_template_attribute",
+        "action_type_add_schema_template",
+        "objects_current_add_fed_key",
+        "objects_current_update_not_null_constraints",
+        "objects_previous_add_fed_key",
+        "objects_previous_update_not_null_constraints",
+        "instruments_add_fed_key",
+        "instrument_translations_update_not_null_constraints",
+        "users_add_fed_key",
+        "user_type_add_federation_user",
+        "actions_add_fed_key",
+        "actions_update_not_null_constraints",
+        "action_types_add_fed_key",
+        "locations_add_fed_key",
+        "locations_update_not_null_constraints",
+        "object_location_assignments_add_fed_key",
+        "object_location_assignments_update_not_null_constraints",
+        "files_add_fed_key",
+        "files_update_not_null_constraints",
+        "comments_add_fed_key",
+        "comments_update_not_null_constraints",
+        "users_update_not_null_constraints",
+        "action_translations_update_not_null_constraints",
+        "markdown_images_add_fed_key",
+        "create_objects_current_name_cache",
+        "create_objects_current_tags_cache",
+        "drop_public_actions",
+        "drop_public_objects",
+        "replace_user_object_permissions_by_all_view",
+        "drop_default_public_permissions",
+        "setup_default_location_permissions",
+        "file_log_add_edit_url_type",
+        "replace_user_object_permissions_by_all_view2",
+        "languages_add_datetime_format_moment_output",
+        "replace_user_object_permissions_by_all_view3",
+        "fed_user_aliases_add_use_real_name_column",
+        "fed_user_aliases_add_use_real_email_column",
+        "fed_user_aliases_add_use_real_orcid_column",
+        "fed_user_aliases_add_use_real_affiliation_column",
+        "fed_user_aliases_add_use_real_role_column",
+        "fed_user_aliases_add_null_if_use_real_data_check",
+        "action_type_add_scicat_export_type",
+        "object_location_assignments_add_declined",
+        "fed_user_aliases_add_last_modified",
+        "components_add_last_sync_timestamp",
+        "users_add_last_modified",
+        "location_types_create_default_location_types",
+        "locations_add_type_id",
+        "location_log_create_initial_log_entries",
+        "notification_type_add_responsibility_assignment_declined",
+        "action_type_add_order_index",
+        "actions_add_admin_only",
+        "actions_add_disable_create_objects",
+        "users_add_last_modified_by_id",
+        "locations_add_is_hidden",
+        "background_tasks_add_result_expiration_date_column",
+        "dataverse_exports_add_status",
+        "location_types_add_enable_instruments",
+        "instruments_add_location_id",
+        "files_fix_fed_unique_constraint",
+        "object_location_assignments_drop_declined_default",
+        "instruments_drop_notes",
+        "object_location_assignments_drop_utc_datetime_not_null_constraint",
+        "locations_drop_is_hidden_default",
+        "languages_change_datetime_format_moment_output_type",
+        "replace_user_object_permissions_by_all_view4",
+        "fed_object_log_entries_add_user_id",
+        "object_shares_add_user_id",
+        "fed_object_log_entry_type_add_remote_object_import",
+        "object_shares_add_import_status",
+        "notification_type_add_remote_object_import_failed",
+        "notification_type_add_remote_object_import_notes",
+        "location_types_add_enable_capacities",
+        "action_translations_add_not_null_constraints",
+        "action_type_translations_add_not_null_constraints",
+        "instrument_translations_add_not_null_constraints",
+        "authentications_add_not_null_constraints",
+        "component_authentications_add_not_null_constraints",
+        "own_component_authentications_add_not_null_constraints",
+        "download_service_job_files_add_not_null_constraints",
+        "two_factor_authentication_methods_add_not_null_constraints",
+        "markdown_images_add_not_null_constraint",
+        "languages_add_not_null_constraints",
+        "action_type_add_enable_instrument_link",
+        "instruments_add_object_id",
+        "instruments_add_show_linked_object_data",
+        "authentication_type_add_api_access_token",
+        "user_type_add_eln_import_user",
+        "object_log_entry_type_add_import_from_eln_file",
+        "objects_previous_add_eln_import",
+        "objects_current_add_eln_import",
+        "users_add_eln_import",
+        "user_log_entry_type_add_import_from_eln_file",
+        "components_add_discoverable",
+        "actions_add_objects_readable_by_all_users_by_default",
+        "files_add_data_not_null_constraint",
+        "replace_timestamp_with_timestamptz",
+        "authentication_type_add_fido2_passkey",
+        "action_type_add_show_in_object_filters",
+        "notification_type_add_automatic_user_federation",
+        "topics_add_short_description",
+        "topics_add_is_markdown",
+        "actions_add_use_instrument_topics",
+        "object_log_add_is_imported",
+        "languages_add_date_format_moment_output",
+        "files_add_preview_image",
+        "group_invitations_add_revoked",
+        "project_invitations_add_revoked",
+    ]
 
-    migrations: typing.Dict[int, typing.Tuple[str, typing.Callable[[typing.Any], bool]]] = {}
-
-    for migration_file in os.listdir(migrations_dir):
-        if not migration_file.endswith('.py'):
-            continue
-        if migration_file.startswith('_'):
-            continue
-        migration_name, _ = os.path.splitext(migration_file)
-        migration_module = importlib.import_module('sampledb.models.migrations.' + migration_name)
+    migrations = []
+    for migration_index, migration_module_name in enumerate(migration_module_names):
         try:
-            migration_index = migration_module.MIGRATION_INDEX
-            migration_name = migration_module.MIGRATION_NAME
+            migration_module = importlib.import_module('sampledb.models.migrations.' + migration_module_name)
             migration_function = migration_module.run
-        except AttributeError:
-            continue
-        # prevent duplicate migration indices
-        if migration_index in migrations:
-            print(f"Duplicate migration index {migration_index} used in {migration_name} and {migrations[migration_index][0]}", file=sys.stderr)
+        except Exception:
+            print(f'Failed to load migration #{migration_index} "{migration_module_name}".', file=sys.stderr)
             sys.exit(1)
-        migrations[migration_index] = (migration_name, migration_function)
-
-    migration_indices = list(sorted(migrations.keys()))
-
-    # ensure no migration index is missing
-    assert migration_indices == list(range(len(migration_indices)))
-
-    sorted_migrations = []
-    for index in migration_indices:
-        name, function = migrations[index]
-        sorted_migrations.append((index, name, function))
-    return sorted_migrations
+        migrations.append((migration_index, migration_module_name, migration_function))
+    return migrations
 
 
 def table_has_column(table_name: str, column_name: str) -> bool:
