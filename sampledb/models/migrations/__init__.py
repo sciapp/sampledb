ba0ca2aa51168af512f37079bb3ec9c539340e14
Florian Rhiem <f.rhiem@fz-juelich.de>
2024-06-10 10:13:04 +0200

    Print migrations without logging module

--- sampledb/models/migrations/__init__.py
+++ sampledb/models/migrations/__init__.py
@@ -2,8 +2,7 @@
 """
 
 """
-
-import logging
+import sys
 
 import flask_sqlalchemy
 
@@ -11,21 +10,18 @@ from .utils import find_migrations, should_skip_by_index, update_migration_index
 
 
 def run(db: flask_sqlalchemy.SQLAlchemy) -> None:
-    logger = logging.getLogger('sampledb.migrations')
     for index, name, function in find_migrations():
-        logger.info('Migration #%d "%s":', index, name)
 
         # Skip migration by migration index
         if should_skip_by_index(db, index):
-            logger.info("Skipped (index).")
             continue
 
         try:
             # Perform migration
             if function(db):
-                logger.info("Done.")
+                print(f'Migration #{index} "{name}" applied.', file=sys.stderr)
             else:
-                logger.info("Skipped (condition).")
+                print(f'Migration #{index} "{name}" skipped.', file=sys.stderr)
 
             # Update migration index to skip this migration by index in the future
             update_migration_index(db, index)
