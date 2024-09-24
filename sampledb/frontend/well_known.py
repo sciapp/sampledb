d9acde1db2a840ede99921125018f18057f7f1e2
Lukas Engering <l.engering@fz-juelich.de>
2024-09-24 10:17:09 +0200

    Add seperate attribute for dateCreated

--- sampledb/frontend/well_known.py
+++ sampledb/frontend/well_known.py
@@ -21,15 +21,16 @@ def minisign_pub_key() -> bytes:
 @frontend.route('/.well-known/keys.json/')
 def key_list_json() -> FlaskResponseT:
     key_pairs = minisign_keys.get_key_pairs()
+    # TODO replace hardcoded URL to localhost
     res = [
         {
             "@context": "https://schema.org",
 	        "@type": "MediaObject",
-            #TODO replace hardcoded URL
             "contentUrl": f"http://localhost:8000/.well-known/keys/{str(kp.id)}",
             "name": f"ed25519_pub_{str(kp.id)}",
             "encodingFormat": "application/x-minisign-key",
-            "description": f"Signing key for exported archives, created: {kp.utc_datetime_created}"
+            "description": f"Signing key for exported archives",
+            "dateCreated": kp.utc_datetime_created
         }
         for kp in key_pairs
     ]
