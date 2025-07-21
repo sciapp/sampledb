e97b981e21c892bb267ec7fcedf91058dacc3e48
Jannik Streitberg <j.streitberg@fz-juelich.de>
2025-07-21 14:08:02 +0200

    Add tests for setting background task result

--- tests/logic/test_background_tasks.py
+++ tests/logic/test_background_tasks.py
@@ -30,6 +30,7 @@ def test_background_tasks(app):
         {'value': 2}
     ]
 
+    sampledb.logic.background_tasks.core.should_stop = False
     app.config['ENABLE_BACKGROUND_TASKS'] = True
 
     handler_return_value = True
@@ -53,5 +54,86 @@ def test_background_tasks(app):
     # other tests should proceed with background tasks off, but that way the
     # handler threads would not be stopped later on, so they need to be
     # stopped now
+    sampledb.logic.background_tasks.stop_handler_threads(app)
+    app.config['ENABLE_BACKGROUND_TASKS'] = False
+
+
+def test_background_tasks_result_success(app):
+    def test_handler_success(data, task_id):
+        return True, {'result': 'success'}
+
+    sampledb.logic.background_tasks.core.should_stop = False
+    app.config['ENABLE_BACKGROUND_TASKS'] = True
+
+    sampledb.logic.background_tasks.core.HANDLERS['test'] = test_handler_success
+
+    task_status, task = sampledb.logic.background_tasks.post_background_task('test', {}, False)
+    assert task_status == sampledb.logic.background_tasks.core.BackgroundTaskStatus.POSTED
+    assert task is not None
+
+    for _ in range(5):
+        db.session.refresh(task)
+        if task.status in (sampledb.logic.background_tasks.core.BackgroundTaskStatus.DONE, sampledb.logic.background_tasks.core.BackgroundTaskStatus.FAILED):
+            break
+        time.sleep(0.1)
+
+    task = sampledb.logic.background_tasks.core.get_background_task(task.id)
+    assert task.status == sampledb.logic.background_tasks.core.BackgroundTaskStatus.DONE
+    assert task.result == {'result': 'success'}
+
+    sampledb.logic.background_tasks.stop_handler_threads(app)
+    app.config['ENABLE_BACKGROUND_TASKS'] = False
+
+
+def test_background_tasks_result_failed(app):
+    def test_handler_fails(data, task_id):
+        return False, {'result': 'failed'}
+
+    sampledb.logic.background_tasks.core.should_stop = False
+    app.config['ENABLE_BACKGROUND_TASKS'] = True
+
+    sampledb.logic.background_tasks.core.HANDLERS['test'] = test_handler_fails
+
+    task_status, task = sampledb.logic.background_tasks.post_background_task('test', {'value': 1}, False)
+    assert task_status == sampledb.logic.background_tasks.core.BackgroundTaskStatus.POSTED
+    assert task is not None
+
+    for _ in range(5):
+        db.session.refresh(task)
+        if task.status in (sampledb.logic.background_tasks.core.BackgroundTaskStatus.DONE, sampledb.logic.background_tasks.core.BackgroundTaskStatus.FAILED):
+            break
+        time.sleep(0.1)
+
+    task = sampledb.logic.background_tasks.core.get_background_task(task.id)
+    assert task.status == sampledb.logic.background_tasks.core.BackgroundTaskStatus.FAILED
+    assert task.result == {'result': 'failed'}
+
+    sampledb.logic.background_tasks.stop_handler_threads(app)
+    app.config['ENABLE_BACKGROUND_TASKS'] = False
+
+
+def test_background_tasks_result_exception(app):
+    def test_handler_exception(data, task_id):
+        raise Exception('test exception')
+
+    sampledb.logic.background_tasks.core.should_stop = False
+    app.config['ENABLE_BACKGROUND_TASKS'] = True
+
+    sampledb.logic.background_tasks.core.HANDLERS['test'] = test_handler_exception
+
+    task_status, task = sampledb.logic.background_tasks.post_background_task('test', {'value': 1}, False)
+    assert task_status == sampledb.logic.background_tasks.core.BackgroundTaskStatus.POSTED
+    assert task is not None
+
+    for _ in range(5):
+        db.session.refresh(task)
+        if task.status in (sampledb.logic.background_tasks.core.BackgroundTaskStatus.DONE, sampledb.logic.background_tasks.core.BackgroundTaskStatus.FAILED):
+            break
+        time.sleep(0.1)
+
+    task = sampledb.logic.background_tasks.core.get_background_task(task.id)
+    assert task.status == sampledb.logic.background_tasks.core.BackgroundTaskStatus.FAILED
+    assert task.result is None
+
     sampledb.logic.background_tasks.stop_handler_threads(app)
     app.config['ENABLE_BACKGROUND_TASKS'] = False
